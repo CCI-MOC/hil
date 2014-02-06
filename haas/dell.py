514a3b7bba5ac6b9699c9be64f411834787fc3fc
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-02-05 21:29:28 -0500

    disabled snmp logging

--- haas/dell.py
+++ haas/dell.py
@@ -6,7 +6,7 @@ def make_remove_vlans(vlan_ids,add,switch_ip='192.168.3.245'):
     # Expects that you send a string which is a comma separated list of vlan_ids and a bool for adding or removing
     for vlan_id in vlan_ids.split(','):
         if add:
-            cmd='''snmpset -v1 -cDell_Network_Manager 192.168.3.245 \
+            cmd='''snmpset -L n -v1 -cDell_Network_Manager 192.168.3.245 \
 dot1qVlanStaticName.%s s "%s" \
 dot1qVlanStaticEgressPorts.%s x '00' \
 dot1qVlanForbiddenEgressPorts.%s x '00' \
@@ -32,7 +32,7 @@ def edit_ports_on_vlan(port_ids,vlan_id,add,switch_ip='192.168.3.245'):
 
     hexstr="%x"%x
     hexstr=hexstr.zfill(6)
-    cmd='''snmpset -v1 -cDell_Network_Manager 192.168.3.245 \
+    cmd='''snmpset -L n -v1 -cDell_Network_Manager 192.168.3.245 \
 dot1qVlanStaticEgressPorts.%s x '%s' \
 dot1qVlanStaticUntaggedPorts.%s x '%s' '''%(vlan_id,hexstr,vlan_id,hexstr)
     print cmd
