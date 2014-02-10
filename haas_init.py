e1f4405a7b925916205979c213a4ba22fc451482
Ian Denhardt <ian@zenhack.net>
2014-02-10 12:56:56 -0500

    Fixed some resource leaks in haas_init.py
    This was bug number #15 in the CCI-MOC/moc-public repo. The haas is no
    longer maintained there; the fix has only been applied to the version in
    CCI-MOC/haas.

--- haas_init.py
+++ haas_init.py
@@ -3,64 +3,69 @@ import haas.config
 import haas.control
 def create_node_pool():
     keys=['node_id','mac_addr','manage_ip']
-
-    for line in open(haas.config.file_names["node"]):
-        values=line.rstrip().split(' ')
-        d=dict(zip(keys,values))
-        session.add(Node(int(d['node_id']),d['mac_addr'],d['manage_ip']))
+    with open(haas.config.file_names["node"]) as file:
+        for line in file:
+            values=line.rstrip().split(' ')
+            d=dict(zip(keys,values))
+            session.add(Node(int(d['node_id']),d['mac_addr'],d['manage_ip']))
     session.commit()
 
 def create_network_pool():
     keys=['network_id','network_technology']
-
-    for line in open(haas.config.file_names["network"]):
-        values=line.rstrip().split(" ")
-        d=dict(zip(keys,values))
-        session.add(Network(int(d["network_id"]),d["network_technology"]))
+    with open(haas.config.file_names["network"]) as file:
+        for line in file:
+            values=line.rstrip().split(" ")
+            d=dict(zip(keys,values))
+            session.add(Network(int(d["network_id"]),d["network_technology"]))
     session.commit()
 
 def create_vm_pool():
     keys=['vm_name']
-    for line in open(haas.config.file_names["vm"]):
-        values = line.rstrip().split(" ")
-        d = dict(zip(keys,values))
-        session.add(VM(d["vm_name"]))
+    with open(haas.config.file_names["vm"]) as file:
+        for line in file:
+            values = line.rstrip().split(" ")
+            d = dict(zip(keys,values))
+            session.add(VM(d["vm_name"]))
     session.commit()
 
 
 def create_switch_pool():
     keys=["switch_id","script"]
-    for line in open(haas.config.file_names["switch"]):
-        values = line.rstrip().split(" ")
-        d = dict(zip(keys,values))
-        session.add(Switch(int(d["switch_id"]),d["script"]))
+    with open(haas.config.file_names["switch"]) as file:
+        for line in file:
+            values = line.rstrip().split(" ")
+            d = dict(zip(keys,values))
+            session.add(Switch(int(d["switch_id"]),d["script"]))
     session.commit()
 
 def create_port_pool():
     keys=["port_id","switch_id","port_no"]
-    for line in open(haas.config.file_names["port"]):
-        values = line.rstrip().split(" ")
-        d = dict(zip(keys,values))
-        session.add(Port(int(d["port_id"]),int(d["switch_id"]),int(d["port_no"])))
+    with open(haas.config.file_names["port"]) as file:
+        for line in file):
+            values = line.rstrip().split(" ")
+            d = dict(zip(keys,values))
+            session.add(Port(int(d["port_id"]),int(d["switch_id"]),int(d["port_no"])))
     session.commit()
 
 def connect_node_to_port():
 
     keys=["node_id","port_id"]
-    for line in open(haas.config.file_names["connect"]):
-        values = line.rstrip().split(" ")
-        d=dict(zip(keys,values))
-        node=haas.control.get_entity_by_cond(Node,"node_id==%d"%int(d["node_id"]))
-        port=haas.control.get_entity_by_cond(Port,"port_id==%d"%int(d["port_id"]))
-        node.port=port
+    with open(haas.config.file_names["connect"]) as file:
+        for line in file:
+            values = line.rstrip().split(" ")
+            d=dict(zip(keys,values))
+            node=haas.control.get_entity_by_cond(Node,"node_id==%d"%int(d["node_id"]))
+            port=haas.control.get_entity_by_cond(Port,"port_id==%d"%int(d["port_id"]))
+            node.port=port
     session.commit()
 
 def add_users():
     keys=["user_name","password","user_type"]
-    for line in open(haas.config.file_names["user"]):
-        values = line.rstrip().split(" ")
-        d = dict(zip(keys,values))
-        session.add(User(d["user_name"],d["password"],d["user_type"]))
+    with open(haas.config.file_names["user"]) as file:
+        for line in open(haas.config.file_names["user"]):
+            values = line.rstrip().split(" ")
+            d = dict(zip(keys,values))
+            session.add(User(d["user_name"],d["password"],d["user_type"]))
     session.commit()
 
 def load_resources():
