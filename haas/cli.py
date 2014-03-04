b4cd910f67b4b2a0a64a74799f5649f50cbbdc2e
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-03-04 13:49:03 -0500

    cli refactor done

--- haas/cli.py
+++ haas/cli.py
@@ -88,35 +88,55 @@ def add_nic(nic_id, node_id):
 
 @command('nic connect (\d+) (\d+)')
 def connect_nic(nic_id, port_id):
-    return control.connect_nic(nic_id, port_id)
+    control.connect_nic(int(nic_id), int(port_id))
 
 @command('switch create (\d+) (\w+)')
 def create_switch(switch_id, script):
     control.create_switch(int(switch_id), script)
 
-create_port = re.compile('^port create (\d+) (\d+) (\d+)$')
-connect_nic = re.compile('^nic connect (\d+) (\d+)$')
-deploy_group = re.compile('^group deploy (\w+)$')
-destroy_group = re.compile('^group destroy (\w+)$')
-create_vlan = re.compile('^vlan create (\d+)$')
-connect_vlan = re.compile('^vlan connect (\d+) (\w+) (\w+)$')
-add_node = re.compile('^node add (\d+) (\w+)$')
-create_node = re.compile('^node create (\d+)$')
-change_head = re.compile('^change (\w+) head to (\S+)')
-change_vlan = re.compile('^change (\w+) vlan to (\d+)')
-show_table = re.compile('^show (\w+)')
-show_free_table = re.compile('^show free (\w+)')
-show_group = re.compile('^show group (\w+)')
-destroy_vlan = re.compile('^destroy vlan (\d+)')
-create_user = re.compile('^create user (\w+) (\w+)$')
-remove = re.compile('^remove (\S+) from (\w+)$')
+@command('port create (\d+) (\d+) (\d+)')
+def create_port(port_id,switch_id,port_no):
+    control.create_port(int(port_id),int(switch_id),int(port_no))
+    
+@command('vlan create (\d+)')
+def create_vlan(vlan_id):
+    control.create_vlan(int(vlan_id))
+    
+@command('vlan connect (\d+) (\w+) (\w+)')
+def connect_vlan(vlan_id,group_name,nic_name):
+    control.connect_vlan(int(vlan_id),group_name,nic_name)
+
+@command('node add (\d+) (\w+)')
+def add_node(node_id,group_name):
+    control.add_node_to_group(int(node_id),group_name)
+
+@command('group deploy (\w+)')
+def deploy_group(group_name):
+    control.destroy_group(group_name)
+    
+@command('user create (\w+) (\w+)')
+def create_user(user_name,password):
+    control.create_user(user_name,password)
+    
 
 @command('headnode attach ([\w-]+) (\w+)')
-def headnode_attach(vlan_id, group_name): pass
+def headnode_attach(vm_name, group_name):
+    control.attach_headnode(vm_name,group_name)
 
+@command('show all')
+def show_all():
+    control.show_all()
+
+@command('show (\w+)')
+def show_table(table_name):
+    control.show_table(table_name)
+
+ 
 @command('exit')
 def exit_cmd():
+    print 'Bye for now.'
     raise _QuitException()
+
 
 @command('help')
 def usage():
@@ -134,7 +154,7 @@ def usage():
         headnode create
         headnode attach <vm_name> <group_name>
         group deploy <group_name>
-
+        show all
         exit
         '''
     print help_text
