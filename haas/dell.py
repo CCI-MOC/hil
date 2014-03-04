ae91a5c80de5cf031dcde2d38db73fba690391b7
Ian Denhardt <ian@zenhack.net>
2014-03-03 20:39:12 -0500

    Added some code to change vlans via serial (powerconnect)
    It isn't currently integrated into the rest of the codebase. Preliminary
    interactive testing suggests that it works.

--- haas/dell.py
+++ haas/dell.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python
 import os
 
-
 def make_remove_vlans(vlan_ids,add,switch_ip='192.168.3.245'):
     # Expects that you send a string which is a comma separated list of vlan_ids and a bool for adding or removing
     for vlan_id in vlan_ids.split(','):
@@ -39,3 +38,30 @@ dot1qVlanStaticEgressPorts.%s x '%s' \
 dot1qVlanStaticUntaggedPorts.%s x '%s' '''%(vlan_id,hexstr,vlan_id,hexstr)
     print cmd
     os.system(cmd)
+
+def set_access_vlan(port, vlan_id):
+    """sets the given port number to access mode, with the given vlan_id."""
+    # TODO: we aren't really doing much in the way of error checking here.
+    def _backout(f):
+        """Back out to the top level prompt."""
+        for _ in range(3):
+            # On the dell, extra exit commands are ignored. we never delve more
+            # than three prompts in, so this should be enough.
+            f.write("exit\n")
+
+    with open('/dev/ttyS0', 'w') as f:
+        _backout(f)
+
+        # find our way to the prompt for this port:
+        f.write("enable\n")
+        f.write("config\n")
+        f.write("int gi1/0/%d\n" % port)
+
+        # set the vlan
+        f.write("sw access vlan %d\n" % vlan_id)
+
+        # set the port to access mode
+        f.write("sw mode access\n")
+ 
+        # Just for good measure:
+        _backout(f)
