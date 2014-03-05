2622919e4e3807ff411c9a46ce4d05144f32f1ff
Ian Denhardt <ian@zenhack.net>
2014-03-05 01:40:45 -0500

    Refactored the help message code.
    The usage function now just prints the docstrings of all the commands;
    the help_text variable has been split up among these docstrings. This is
    better since it keeps the relevant documentation near the thing it
    documents.

--- haas/cli.py
+++ haas/cli.py
@@ -28,6 +28,9 @@ def command(pattern):
     Subexpressions enclosed in parentheses will be passed to the
     function as positional arguments when the command is executed.
 
+    The docstring of each command will be displayed as part of the help
+    message.
+
     This decorator does not modify its argument.
     """
     pattern = '^' + pattern + '$'
@@ -75,93 +78,92 @@ def main_loop():
 
 @command('group create (\w+)')
 def create_group(group_name):
+    """group create <group_name>"""
     control.create_group(group_name)
 
 @command('node create (\d+)')
 def create_node(node_id):
+    """node create <node_id>"""
     control.create_node(node_id)
 
 @command('node add (\d+) (\w+)')
 def add_node(node_id,group_name):
+    """node add <node_id> <group_name>"""
     control.add_node_to_group(int(node_id),group_name)
 
 @command('nic create (\d+) (\w+) (\w+)')
 def create_nic(nic_id, mac_addr, name):
+    """nic create <nic_id> <mac_addr> <name>"""
     control.create_nic(int(nic_id), mac_addr, name)
 
 @command('nic connect (\d+) (\d+)')
 def connect_nic(nic_id, port_id):
+    """nic connect <nic_id> <port_id>"""
     control.connect_nic(int(nic_id), int(port_id))
 
 @command('nic add (\d+) (\d+)')
 def add_nic(nic_id, node_id):
+    """nic add <nic_id> <node_id>"""
     control.add_nic(int(nic_id), int(node_id))
 
 @command('switch create (\d+) (\w+)')
 def create_switch(switch_id, script):
+    """switch create <switch_id> <script>"""
     control.create_switch(int(switch_id), script)
 
 @command('port create (\d+) (\d+) (\d+)')
 def create_port(port_id,switch_id,port_no):
+    """port create <port_id> <switch_id> <port_no>"""
     control.create_port(int(port_id),int(switch_id),int(port_no))
     
 @command('vlan create (\d+)')
 def create_vlan(vlan_id):
+    """vlan create <vlan_id>"""
     control.create_vlan(int(vlan_id))
     
 @command('vlan connect (\d+) (\w+) (\w+)')
 def connect_vlan(vlan_id,group_name,nic_name):
+    """vlan connect <vlan_id> <group_name> <nic_name>"""
     control.connect_vlan(int(vlan_id),group_name,nic_name)
 
 @command('headnode create')
 def headnode_create():
+    """headnode create"""
     control.create_headnode()
 
 @command('headnode attach ([\w-]+) (\w+)')
 def headnode_attach(vm_name, group_name):
+    """headnode attach <vm_name> <group_name>"""
     control.attach_headnode(vm_name,group_name)
 
 @command('group deploy (\w+)')
 def deploy_group(group_name):
+    """group deploy <group_name>"""
     control.destroy_group(group_name)
     
 @command('user create (\w+) (\w+)')
 def create_user(user_name,password):
+    """user create <user name> <password>"""
     control.create_user(user_name,password)
 
 @command('show all')
 def show_all():
+    """show all"""
     control.show_all()
 
 @command('show (\w+)')
 def show_table(table_name):
+    """show [ group | vm | port | nic | vlan | switch | node | user ]"""
     control.show_table(table_name)
 
 @command('help')
 def usage():
-    help_text='''
-        group create <group_name>
-        node create <node_id>
-        node add <node_id> <group_name>
-        nic create <nic_id> <mac_addr> <name>
-        nic connect <nic_id> <port_id>
-        nic add <nic_id> <node_id>
-        switch create <switch_id> <script>
-        port create <port_id> <switch_id> <port_no>
-        vlan create <vlan_id>
-        vlan connect <vlan_id> <group_name> <nic_name>
-        headnode create
-        headnode attach <vm_name> <group_name>
-        group deploy <group_name>
-        user create <user name> <password>
-        show all
-        show [ group | vm | port | nic | vlan | switch | node | user ]
-        help
-        exit
-        '''
-    print help_text
+    """help"""
+    for _, func in commands:
+        print func.__doc__
 
 @command('exit')
 def exit_cmd():
+    """exit"""
     print 'Bye for now.'
     raise _QuitException()
