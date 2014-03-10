fba28a6eddd0eb644b6c3e4501a76e452597e30d
Ian Denhardt <ian@zenhack.net>
2014-03-10 08:12:05 -0400

    Refactored the way commands are parsed.
    This has several advantages:
      - It fixes #22.
      - More generally, the shell is less sensitive to input variations; the
        old one would choke on leading/trailing whitespace, or extra
        whitespace. Now the parser just sees whitespace separated strings.
      - We no longer have to duplicate the names of command and their
        arguments; these are derived from the python function
        names/arguments. We can still override the name when we need to
        though.
    
    We take advantage of the last point in a number of places, so the
    command names have changed.
    
    Still on the TODO list, the docstrings are no longer used, and contain
    no useful information that isn't in the immediate vicinity. We could
    should use this to provide a brief description of what the function does
    - which we could even add to the help message.

--- haas/cli.py
+++ haas/cli.py
@@ -1,157 +1,150 @@
 
 from haas import control
-import re
+import inspect
 
-# A list of tuples of the form `(matcher, func)`, where `func` is a
-# function to run when a command matching matcher is executed. Commands
-# are added via the command decorator
-commands = []
+# A dictionary with names of commands (strings) as keys, and the corresponding
+# functions as values. See the `command` decorator, below, for details.
+commands = {}
 
 
 # This is used by the exit command to tell the main loop when to stop.
 class _QuitException(Exception): pass
 
-def command(pattern):
+def command(name=None):
     """`command` is a decorator used to define shell commands.
 
-    Defining a command looks like:
-
-        @command('example command ([a-z]+) ([0-9]+)')
-        def example_command(word, number):
+        @command()
+        def do_foo(x,y,z):
             ...
 
-    `pattern` should be a regex pattern which matches the desired
-    command syntax. The pattern must match the whole command, which must
-    fit on a single line - the pattern `create foo` will not match the
-    command string `prefix create foo` or `create foo suffix`.
+    Will register a command named `do_foo`, which expects three arguments, and
+    invokes the function do_foo when executed. when the help command is
+    executed, the string "do_foo <x> <y> <z>" will be shown.
 
-    Subexpressions enclosed in parentheses will be passed to the
-    function as positional arguments when the command is executed.
+    An optional parameter may be provided, which will override the name of the
+    command. This is useful (for example) when the command has the same name as
+    a standard python function:
 
-    The docstring of each command will be displayed as part of the help
-    message.
+        @command('help')
+        def usage():
+            ...
 
     This decorator does not modify its argument.
     """
-    pattern = '^' + pattern + '$'
-    matcher = re.compile(pattern)
     def register(func):
-        commands.append((matcher, func))
+        if name:
+            commands[name] = func
+        else:
+            commands[func.__name__] = func
         return func
     return register
 
-def run_command(args):
-    """Runs the command specified in the string `args`.
+def run_command(cmd):
+    """Run the command specified in the string `cmd`.
 
-    returns `False` if the argument is not a valid command, and `True`
-    otherwise.
+    if `cmd` is not a valid command, display a help message to the user.
     """
-    for matcher, func in commands:
-        match = matcher.match(args)
-        if not match:
-            continue
-        func(*match.groups())
-        return True
-    return False 
-
-def usage():
-    pass
+    parts = cmd.split()
+    if parts[0] not in commands:
+        print('Invalid command.')
+        usage()
+    else:
+        func = commands[parts[0]]
+        func(*parts[1:])
 
 def main_loop():
     """Runs the interactive command interpreter.
 
     Repeatedly prompts the user for commands and executes them. displays
     usage information if an invalid command is entered, and stops when
-    the `exit` command is executed.
+    the `exit` command is executed, or end of file is reached.
     """
     try:
         while True:
             cmd = raw_input('haas> ')
-            if not run_command(cmd):
-                print "Invalid command."
-                usage()
+            run_command(cmd)
     except _QuitException:
         pass
     except EOFError:
         # We should exit cleanly on EOF
         pass
 
-@command('group create (\w+)')
+@command()
 def create_group(group_name):
     """group create <group_name>"""
     control.create_group(group_name)
 
-@command('node create (\d+)')
+@command()
 def create_node(node_id):
     """node create <node_id>"""
     control.create_node(node_id)
 
-@command('node add (\d+) (\w+)')
+@command()
 def add_node(node_id,group_name):
     """node add <node_id> <group_name>"""
     control.add_node_to_group(int(node_id),group_name)
 
-@command('nic create (\d+) (\w+) (\w+)')
+@command()
 def create_nic(nic_id, mac_addr, name):
     """nic create <nic_id> <mac_addr> <name>"""
     control.create_nic(int(nic_id), mac_addr, name)
 
-@command('nic connect (\d+) (\d+)')
+@command()
 def connect_nic(nic_id, port_id):
     """nic connect <nic_id> <port_id>"""
     control.connect_nic(int(nic_id), int(port_id))
 
-@command('nic add (\d+) (\d+)')
+@command()
 def add_nic(nic_id, node_id):
     """nic add <nic_id> <node_id>"""
     control.add_nic(int(nic_id), int(node_id))
 
-@command('switch create (\d+) (\w+)')
+@command()
 def create_switch(switch_id, script):
     """switch create <switch_id> <script>"""
     control.create_switch(int(switch_id), script)
 
-@command('port create (\d+) (\d+) (\d+)')
+@command()
 def create_port(port_id,switch_id,port_no):
     """port create <port_id> <switch_id> <port_no>"""
     control.create_port(int(port_id),int(switch_id),int(port_no))
     
-@command('vlan create (\d+)')
+@command()
 def create_vlan(vlan_id):
     """vlan create <vlan_id>"""
     control.create_vlan(int(vlan_id))
     
-@command('vlan connect (\d+) (\w+) (\w+)')
+@command()
 def connect_vlan(vlan_id,group_name,nic_name):
     """vlan connect <vlan_id> <group_name> <nic_name>"""
     control.connect_vlan(int(vlan_id),group_name,nic_name)
 
-@command('headnode create')
+@command()
 def headnode_create():
     """headnode create"""
     control.create_headnode()
 
-@command('headnode attach ([\w-]+) (\w+)')
+@command()
 def headnode_attach(vm_name, group_name):
     """headnode attach <vm_name> <group_name>"""
     control.attach_headnode(vm_name,group_name)
 
-@command('group deploy (\w+)')
+@command()
 def deploy_group(group_name):
     """group deploy <group_name>"""
     control.deploy_group(group_name)
     
-@command('user create (\w+) (\w+)')
+@command()
 def create_user(user_name,password):
     """user create <user name> <password>"""
     control.create_user(user_name,password)
 
-@command('show all')
+@command()
 def show_all():
     """show all"""
     control.show_all()
 
-@command('show (\w+)')
+@command('show')
 def show_table(table_name):
     """show [ group | vm | port | nic | vlan | switch | node | user ]"""
     control.show_table(table_name)
@@ -160,8 +153,12 @@ def show_table(table_name):
 def usage():
     """help"""
     print "------- commands --------"
-    for _, func in commands:
-        print "   ", func.__doc__
+    names = commands.keys()
+    names.sort()
+    for cmd_name in names:
+        func = commands[cmd_name]
+        arg_names = inspect.getargspec(func).args
+        print "   ", cmd_name, ' '.join(map(lambda x: '<%s>' % x, arg_names))
     print "------ notes ------------"
     print " port_id - global number for port on switch"
     print " port_no - switch specific number of port"
