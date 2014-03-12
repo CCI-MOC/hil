23e9553726b0744879764b68ebe9abb5c52710c5
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-03-12 17:34:08 -0400

    revert to delete read_script command

--- haas_shell.py
+++ haas_shell.py
@@ -10,12 +10,6 @@ def auth(user_name,password):
         return False
     return user.password == password
 
-user = haas.control.get_entity_by_cond(haas.model.User,'user_name=="%s"'%('admin'))
-if user == None:
-    haas.control.create_user('admin','admin')
-    print 'No admin user, created one by default'
-
-
 while True:
     user_name = raw_input('user: ')
     password = getpass.getpass("Password: ")
