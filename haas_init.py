8908de4799ace4db19f53a80acb251a5be0a4cd8
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-03-12 17:16:22 -0400

    haas_init.py<script
    Added the pipe way of initializing db
    haas_init does not ask for authentication

--- haas_init.py
+++ haas_init.py
@@ -1,81 +1,11 @@
-from haas.model import *
-import haas.config
 import haas.control
-def create_node_pool():
-    keys=['node_id','mac_addr','manage_ip']
-    with open(haas.config.file_names["node"]) as file:
-        for line in file:
-            values=line.rstrip().split(' ')
-            d=dict(zip(keys,values))
-            session.add(Node(int(d['node_id']),d['mac_addr'],d['manage_ip']))
-    session.commit()
-
-def create_network_pool():
-    keys=['network_id','network_technology']
-    with open(haas.config.file_names["network"]) as file:
-        for line in file:
-            values=line.rstrip().split(" ")
-            d=dict(zip(keys,values))
-            session.add(Network(int(d["network_id"]),d["network_technology"]))
-    session.commit()
-
-def create_vm_pool():
-    keys=['vm_name']
-    with open(haas.config.file_names["vm"]) as file:
-        for line in file:
-            values = line.rstrip().split(" ")
-            d = dict(zip(keys,values))
-            session.add(VM(d["vm_name"]))
-    session.commit()
+import haas.model
+import haas.cli
 
 
-def create_switch_pool():
-    keys=["switch_id","script"]
-    with open(haas.config.file_names["switch"]) as file:
-        for line in file:
-            values = line.rstrip().split(" ")
-            d = dict(zip(keys,values))
-            session.add(Switch(int(d["switch_id"]),d["script"]))
-    session.commit()
+user = haas.control.get_entity_by_cond(haas.model.User,'user_name=="%s"'%('admin'))
+if user == None:
+    haas.control.create_user('admin','admin')
+    print 'No admin user, created one by default'
 
-def create_port_pool():
-    keys=["port_id","switch_id","port_no"]
-    with open(haas.config.file_names["port"]) as file:
-        for line in file:
-            values = line.rstrip().split(" ")
-            d = dict(zip(keys,values))
-            session.add(Port(int(d["port_id"]),int(d["switch_id"]),int(d["port_no"])))
-    session.commit()
-
-def connect_node_to_port():
-
-    keys=["node_id","port_id"]
-    with open(haas.config.file_names["connect"]) as file:
-        for line in file:
-            values = line.rstrip().split(" ")
-            d=dict(zip(keys,values))
-            node=haas.control.get_entity_by_cond(Node,"node_id==%d"%int(d["node_id"]))
-            port=haas.control.get_entity_by_cond(Port,"port_id==%d"%int(d["port_id"]))
-            node.port=port
-    session.commit()
-
-def add_users():
-    keys=["user_name","password","user_type"]
-    with open(haas.config.file_names["user"]) as file:
-        for line in file:
-            values = line.rstrip().split(" ")
-            d = dict(zip(keys,values))
-            session.add(User(d["user_name"],d["password"],d["user_type"]))
-    session.commit()
-
-def load_resources():
-#    create_node_pool()
-#    create_network_pool()
-#    create_vm_pool()
-#    create_switch_pool()
-#    create_port_pool()
-#    connect_node_to_port()
-    add_users()
-
-if __name__=='__main__':
-    load_resources()
+haas.cli.main_loop()
