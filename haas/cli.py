498ce225aa00a3702d33f8c01f5ad714de527bb1
Ian Denhardt <ian@zenhack.net>
2014-03-13 12:45:16 -0400

    deleted most of the command docstrings
    They provide nothing other than function name & args, and are no longer
    needed to generate the help message. See also issue #34.
    
    I left the one for `show <table_name>`, since it actually provides some
    additional information.

--- haas/cli.py
+++ haas/cli.py
@@ -130,77 +130,62 @@ def main_loop():
 
 @command()
 def create_group(group_name):
-    """group create <group_name>"""
     control.create_group(group_name)
 
 @command()
 def create_node(node_id):
-    """node create <node_id>"""
     control.create_node(node_id)
 
 @command()
 def add_node(node_id,group_name):
-    """node add <node_id> <group_name>"""
     control.add_node_to_group(int(node_id),group_name)
 
 @command()
 def create_nic(nic_id, mac_addr, name):
-    """nic create <nic_id> <mac_addr> <name>"""
     control.create_nic(int(nic_id), mac_addr, name)
 
 @command()
 def connect_nic(nic_id, port_id):
-    """nic connect <nic_id> <port_id>"""
     control.connect_nic(int(nic_id), int(port_id))
 
 @command()
 def add_nic(nic_id, node_id):
-    """nic add <nic_id> <node_id>"""
     control.add_nic(int(nic_id), int(node_id))
 
 @command()
 def create_switch(switch_id, script):
-    """switch create <switch_id> <script>"""
     control.create_switch(int(switch_id), script)
 
 @command()
 def create_port(port_id,switch_id,port_no):
-    """port create <port_id> <switch_id> <port_no>"""
     control.create_port(int(port_id),int(switch_id),int(port_no))
     
 @command()
 def create_vlan(vlan_id):
-    """vlan create <vlan_id>"""
     control.create_vlan(int(vlan_id))
     
 @command()
 def connect_vlan(vlan_id,group_name,nic_name):
-    """vlan connect <vlan_id> <group_name> <nic_name>"""
     control.connect_vlan(int(vlan_id),group_name,nic_name)
 
 @command()
 def headnode_create():
-    """headnode create"""
     control.create_headnode()
 
 @command()
 def headnode_attach(vm_name, group_name):
-    """headnode attach <vm_name> <group_name>"""
     control.attach_headnode(vm_name,group_name)
 
 @command()
 def deploy_group(group_name):
-    """group deploy <group_name>"""
     control.deploy_group(group_name)
     
 @command()
 def create_user(user_name,password):
-    """user create <user name> <password>"""
     control.create_user(user_name,password)
 
 @command()
 def show_all():
-    """show all"""
     control.show_all()
 
 @command('show')
@@ -210,7 +195,6 @@ def show_table(table_name):
 
 @command('help')
 def usage():
-    """help"""
     print "------- commands --------"
     names = commands.keys()
     names.sort()
@@ -224,6 +208,5 @@ def usage():
 
 @command('exit')
 def exit_cmd():
-    """exit"""
     print 'Bye for now.'
     raise _QuitException()
