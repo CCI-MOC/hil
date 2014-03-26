ca345b644be0fd5aa4d9080bf1b4b3bc35e19afe
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-03-26 13:22:05 -0400

    remove formatted string for get_entity_by_cond updated db_init_script_example to conform to new command syntax

--- haas_init.py
+++ haas_init.py
@@ -1,5 +1,5 @@
 from haas import control, model, cli
-user = control.get_entity_by_cond(model.User,'user_name=="%s"'%('admin'))
+user = control.get_entity_by_cond(model.User,'user_name=="admin"')
 if user == None:
     control.create_user('admin','admin')
     print 'No admin user, created one by default'
