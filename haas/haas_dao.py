9fd6e4e2a3e271264811691e4a1e4b97dc1d8f98
LinZhaoliang <liuzhaoliang1988@gmail.com>
2014-05-06 13:59:29 -0400

    Delete haas_dao.py

--- haas/haas_dao.py
+++
@@ -1,9 +0,0 @@
-session = None
-def init_session(s):
-    global session
-    session = s
-def get(cls, label):
-    return session.query(cls).filter_by(label=label).all()
-def save(e):
-    session.add(e)
-    session.commit()
