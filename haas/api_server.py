8f16667d8719a04b926f5b8df0de9add901c78d4
Ian Denhardt <ian@zenhack.net>
2014-05-21 12:58:51 -0400

    Create/delete users, and change passwords via the rest api.
    There are still some design issues to work out; see the comments.
    Also, we're not doing authentication/authorization at all yet.

--- haas/api_server.py
+++ haas/api_server.py
@@ -7,7 +7,7 @@ The api server is meant to be run as a stand-alone command, e.g:
 This is all WIP, of course.
 """
 
-from flask import Flask
+from flask import Flask, request
 from haas import config, model
 
 
@@ -17,6 +17,43 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, HaaS!'
+
+
+@app.route('/user/<username>', methods=['PUT', 'DELETE'])
+def user(username):
+    """Handle create/modify/delete user commands.
+
+    * A delete request will delete a user. If the user does not exists, a 404
+      http status code will be returned.
+    * A put request will create a user if it does not exist, and set the user's
+      password otherwise.
+
+    Right now the client doesn't get any feedback as to whether the user already
+    exists or not. I don't really like the way this works right now, will have
+    to sit down and come up with a proper design.
+    """
+    # It would be nice to have two separate functions for this. All the examples
+    # in the flask docs use a simple if/else like below for this kind of thing,
+    # however.
+    db = model.Session()
+    user = db.query(model.User).filter_by(label = username).first()
+
+    if request.method == 'PUT':
+        password = request.form['password']
+        if not user:
+            user = model.User(username, password)
+        else:
+            user.set_password(password)
+        db.add(user)
+    else: # DELETE
+        if not user:
+            return 'No such user', 404
+        else:
+            db.delete(user)
+
+    db.commit()
+    return ''
+
 
 if __name__ == '__main__':
     config.load()
