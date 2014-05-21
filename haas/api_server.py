4472548cd7355c9fe0d8115ff367d00a1b991bbb
Ian Denhardt <ian@zenhack.net>
2014-05-21 11:46:32 -0400

    Very beginnings of a REST api server.
    Really, this is just a skeleton of a flask app, plus some database
    intialization.

---
+++ haas/api_server.py
@@ -0,0 +1,24 @@
+"""This is the HaaS API server - it provides the HaaS's rest api.
+
+The api server is meant to be run as a stand-alone command, e.g:
+
+    python -m haas.api_server
+
+This is all WIP, of course.
+"""
+
+from flask import Flask
+from haas import config, model
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, HaaS!'
+
+if __name__ == '__main__':
+    config.load()
+    model.init_db(create=True)
+    app.run()
