1253a09f237dcf8381a041094516459a72fd12cc
Ian Denhardt <ian@zenhack.net>
2014-05-28 17:28:23 -0400

    Factored out the check for an existing user.
    We'll want this for most of our *_create functions.
    
    This includes some basic tests, which are passing.

--- haas/api.py
+++ haas/api.py
@@ -20,18 +20,14 @@ class NotFoundError(APIError):
 class DuplicateError(APIError):
     """An exception indicating that a given resource already exists.""" 
 
+
 def user_create(username, password):
     """Create user `username`.
 
     If the user already exists, a DuplicateError will be raised.
     """
     db = model.Session()
-    # XXX: This works, but we should probably have the database do the heavy
-    # lifting, i.e. try to create a user, and if we get an IntegretyError from
-    # sqlalchemy, then pass on the error.
-    user = db.query(model.User).filter_by(label = username).first()
-    if user:
-        raise DuplicateError(username)
+    _assert_absent(db, model.User, username)
     user = model.User(username, password)
     db.add(user)
     db.commit()
@@ -47,3 +43,18 @@ def user_delete(username):
         raise NotFoundError(username)
     db.delete(user)
     db.commit()
+
+def _assert_absent(session, cls, name):
+    """Raises a DuplicateError if the given object is already in the database.
+
+    This is useful for most of the *_create functions.
+
+    Arguments:
+
+    session - a sqlaclhemy session to use.
+    cls - the class of the object to query.
+    name - the name of the object in question.
+    """
+    obj = session.query(cls).filter_by(label = name).first()
+    if obj:
+        raise DuplicateError(cls.__name__ + ': ' + name)
