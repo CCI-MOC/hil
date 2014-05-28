6b4f71148d3cb3bbbfdf71830662bd52e381a35a
jlbernard <bernard.logan4@gmail.com>
2014-05-28 14:02:32 -0400

    changed function names, syntax errors

--- haas/api.py
+++ haas/api.py
@@ -5,7 +5,7 @@ haas.api_server translates between this and HTTP.
 TODO: Spec out and document what sanitization is required.
 """
 
-from haas import model
+import model
 
 class APIError(Exception):
     """An exception indicating an error that should be reported to the user.
@@ -36,7 +36,7 @@ def user_create(username, password):
     db.add(user)
     db.commit()
 
-def user_delete(username):
+def user_destroy(username):
     """Delete user `username`
 
     If the user does not exist, a NotFoundError will be raised.
@@ -68,7 +68,8 @@ def group_create(groupname):
     db.add(group)
     db.commit()
 
-def group_delete(groupname):
+
+def group_destroy(groupname):
     """Delete group 'groupname'
 
     If the group does not exist, a NotFoundError will be raised.
@@ -80,7 +81,8 @@ def group_delete(groupname):
     db.delete(group)
     db.commit()
 
-def add_user(groupname, username):
+
+def group_add_user(groupname, username):
     """Add a group to a user
     
     If the group or user does not exist, a NotFoundError will be raised.
@@ -92,12 +94,13 @@ def add_user(groupname, username):
     group = db.query(model.Group).filter_by(label = groupname).first()
     if not group:
         raise NotFoundError(groupname)
-    if user.groups(group) # if user was already added to group
+    if user.groups(group): # if user was already added to group
         raise DuplicateError(username)
-    user.groups.append(user)
+    user.groups.append(group)
     db.commit()
 
-def remove_user(groupname, username):
+
+def group_remove_user(groupname, username):
     """Remove a group from a user
     
     If the group or user does not exist, a NotFoundError will be raised.
@@ -109,11 +112,46 @@ def remove_user(groupname, username):
     group = db.query(model.Group).filter_by(label = groupname).first()
     if not group:
         raise NotFoundError(groupname)
-    if not user.groups(user) # if user isn't a part of the group
+    if not user.groups(user): # if user isn't a part of the group
         raise NotFoundError(username)    
-    user.groups.remove(user)
+    user.groups.remove(group)
     db.commit()
 
+
+def group_connect_project(groupname, projectname):
+    """Add a project 'projectname' to an existing group
+    
+    If the group or project does not exist, a NotFoundError will be raised.
+    """
+    db = model.Session()
+    project = db.query(model.Project).filter_by(label = projectname).first()
+    if not project:
+        raise NotFoundError(projectname)
+    group = db.query(model.Group).filter_by(label = groupname).first()
+    if not group:
+        raise NotFoundError(groupname)
+    if group.projects(project): # if project was already added to group
+        raise DuplicateError(projectname)
+    project.group.append(group)
+    db.commit()
+
+
+def group_detach_project(groupname, projectname):
+    """Remove a project 'projectname' from an existing group
+    
+    If the group or project does not exist, a NotFoundError will be raised.
+    """
+    db = model.Session()
+    project = db.query(model.Project).filter_by(label = projectname).first()
+    if not project:
+        raise NotFoundError(projectname)
+    group = db.query(model.Group).filter_by(label = groupname).first()
+    if not group:
+        raise NotFoundError(groupname)
+    if not group.projects(project): # if project isn't a part of the group
+        raise NotFoundError(projectname)
+    project.group.remove(group)
+    db.commit()
 
 
                             # Project Code #
@@ -135,7 +173,8 @@ def project_create(projectname):
     db.add(project)
     db.commit()
 
-def project_delete(projectname):
+
+def project_destroy(projectname):
     """Delete project 'projectname'
 
     If the project does not exist, a NotFoundError will be raised.
@@ -147,41 +186,77 @@ def project_delete(projectname):
     db.delete(project)
     db.commit()
 
-# project takes project label
-def add_group(groupname, projectname):
-    """Add a project 'projectname' to an existing group
+
+def project_connect_node(projectname, nodename):
+    """Add a project 'projectname' to an existing node
     
-    If the group or project does not exist, a NotFoundError will be raised.
+    If the node or project does not exist, a NotFoundError will be raised.
     """
     db = model.Session()
     project = db.query(model.Project).filter_by(label = projectname).first()
     if not project:
         raise NotFoundError(projectname)
-    group = db.query(model.Group).filter_by(label = groupname).first()
-    if not group:
-        raise NotFoundError(groupname)
-    if group.projects(project) # if project was already added to group
+    node = db.query(model.Node).filter_by(label = nodename).first()
+    if not node:
+        raise NotFoundError(nodename)
+    if node.projects(project): # if project was already added to node
         raise DuplicateError(projectname)
-    project.group.append(group)
+    project.node.append(node)
     db.commit()
 
-def remove_group(groupname, projectname):
-    """Remove a project 'projectname' from an existing group
+
+def project_detach_node(projectname, nodename):
+    """Remove a project 'projectname' from an existing node
     
-    If the group or project does not exist, a NotFoundError will be raised.
+    If the node or project does not exist, a NotFoundError will be raised.
     """
     db = model.Session()
     project = db.query(model.Project).filter_by(label = projectname).first()
     if not project:
         raise NotFoundError(projectname)
-    group = db.query(model.Group).filter_by(label = groupname).first()
-    if not group:
-        raise NotFoundError(groupname)
-    if not group.projects(project) # if project isn't a part of the group
+    node = db.query(model.Node).filter_by(label = nodename).first()
+    if not node:
+        raise NotFoundError(nodename)
+    if not node.projects(project): # if project isn't a part of the node
         raise NotFoundError(projectname)
-    project.group.remove(group)
+    project.node.remove(node)
     db.commit()
 
+
+def project_connect_network(projectname, networkname):
+    """Add a project 'projectname' to an existing network
+    
+    If the network or project does not exist, a NotFoundError will be raised.
+    """
+    db = model.Session()
+    project = db.query(model.Project).filter_by(label = projectname).first()
+    if not project:
+        raise NotFoundError(projectname)
+    network = db.query(model.Network).filter_by(label = networkname).first()
+    if not network:
+        raise NotFoundError(networkname)
+    if network.projects(project): # if project was already added to network
+        raise DuplicateError(projectname)
+    project.network.append(network)
+    db.commit()
+
+
+def project_detach_network(projectname, networkname):
+    """Remove a project 'projectname' from an existing network
+    
+    If the network or project does not exist, a NotFoundError will be raised.
+    """
+    db = model.Session()
+    project = db.query(model.Project).filter_by(label = projectname).first()
+    if not project:
+        raise NotFoundError(projectname)
+    network = db.query(model.Network).filter_by(label = networkname).first()
+    if not network:
+        raise NotFoundError(networkname)
+    if not network.projects(project): # if project isn't a part of the network
+        raise NotFoundError(projectname)
+    project.network.remove(network)
+    db.commit()
 
 
                             # Node Code #
@@ -203,7 +278,8 @@ def node_create(nodename):
     db.add(node)
     db.commit()
 
-def node_delete(nodename):
+
+def node_destroy(nodename):
     """Delete node 'nodename'
 
     If the node does not exist, a NotFoundError will be raised.
@@ -215,36 +291,36 @@ def node_delete(nodename):
     db.delete(node)
     db.commit()
 
-def add_node(nodename, projectname):
-    """Add a project 'projectname' to an existing node
-    
-    If the node or project does not exist, a NotFoundError will be raised.
+
+                            # Network Code #
+                            ################
+
+def network_create(networkname):
+    """Create network 'networkname'.
+
+    If the network already exists, a DuplicateError will be raised.
     """
     db = model.Session()
-    project = db.query(model.Project).filter_by(label = projectname).first()
-    if not project:
-        raise NotFoundError(projectname)
-    node = db.query(model.Node).filter_by(label = nodename).first()
-    if not node:
-        raise NotFoundError(nodename)
-    if node.projects(project) # if project was already added to node
-        raise DuplicateError(projectname)
-    project.node.append(node)
+    # XXX: This works, but we should probably have the database do the heavy
+    # lifting, i.e. try to create a user, and if we get an IntegretyError from
+    # sqlalchemy, then pass on the error.
+    network = db.query(model.Network).filter_by(label = networkname).first()
+    if network:
+        raise DuplicateError(networkname)
+    network = model.Network(networkname)
+    db.add(network)
     db.commit()
 
-def remove_node(nodename, projectname):
-    """Remove a project 'projectname' from an existing node
-    
-    If the node or project does not exist, a NotFoundError will be raised.
+
+def network_destroy(networkname):
+    """Delete network 'networkname'
+
+    If the network does not exist, a NotFoundError will be raised.
     """
     db = model.Session()
-    project = db.query(model.Project).filter_by(label = projectname).first()
-    if not project:
-        raise NotFoundError(projectname)
-    node = db.query(model.Node).filter_by(label = nodename).first()
-    if not node:
-        raise NotFoundError(nodename)
-    if not node.projects(project) # if project isn't a part of the node
-        raise NotFoundError(projectname)
-    project.node.remove(node)
+    network = db.query(model.Network).filter_by(label = networkname).first()
+    if not network:
+        raise NotFoundError(networkname)
+    db.delete(network)
     db.commit()
+
