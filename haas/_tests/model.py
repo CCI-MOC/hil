398db28e6b7ec7a40ab8ccf6061500dd44713c08
Ian Denhardt <ian@zenhack.net>
2014-06-02 16:13:05 -0400

    A bit of refactoring in the unit tests for haas.model.
    In particular:
    
    * Database setup is now handled as part of a superclass. This will make
      it easier to add additional test cases.
    * More docstrings.

--- haas/_tests/model.py
+++ haas/_tests/model.py
@@ -5,7 +5,10 @@ import tempfile
 import unittest
 
 class ModelTest(unittest.TestCase):
+    """A base class for our model test cases.
 
+    Right now this just abstracts away the logic to set up a new database.
+    """
     def setUp(self):
         tmp = tempfile.mkdtemp()
         os.chdir(tmp)
@@ -17,6 +20,11 @@ uri = sqlite:///:memory:
         config.load()
         init_db(create=True)
 
+class UserMethodTest(ModelTest):
+    """Tests the User class's logic which is independent from the database. """
+
     def test_user_create_verify(self):
+        """Basic test of password verification."""
+
         user = User('bob', 'secret')
         self.assertTrue(user.verify_password('secret'))
