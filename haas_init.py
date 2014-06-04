b64ed0a0c32bf6dffb6b969425255d42f2682a95
Ian Denhardt <ian@zenhack.net>
2014-06-04 16:37:25 -0400

    Deleted a bunch of dead/bit-rotted/deprecated code.
    Everything that remains actually imports cleanly.

--- haas_init.py
+++
@@ -1,7 +0,0 @@
-from haas import control, model, cli
-user = control.get_entity_by_cond(model.User,'user_name=="admin"')
-if user == None:
-    control.create_user('admin','admin')
-    print 'No admin user, created one by default'
-
-cli.main_loop()
