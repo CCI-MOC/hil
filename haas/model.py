5a51aea54647d09b85487760a98982930df4e9a8
Ian Denhardt <ian@zenhack.net>
2014-06-13 16:20:10 -0400

    Trailing whitespace

--- haas/model.py
+++ haas/model.py
@@ -43,11 +43,11 @@ class Nic(Model):
 
     port_label   = Column(Integer,ForeignKey('port.label'))
     node_label   = Column(Integer,ForeignKey('node.label'))
-    
+
     # One to one mapping port
     port      = relationship("Port",backref=backref('nic',uselist=False))
-    node      = relationship("Node",backref=backref('nics',order_by=label)) 
-    
+    node      = relationship("Node",backref=backref('nics',order_by=label))
+
     def __init__(self,label, mac_addr):
         self.label     = label
         self.mac_addr  = mac_addr
@@ -57,14 +57,14 @@ class Nic(Model):
                                       self.mac_addr,
                                       self.port_label if self.port else None,
                                       self.node_label if self.node else None)
-            
+
 class Node(Model):
 
     id            = Column(Integer,primary_key=True)
     label         = Column(String)
     available     = Column(Boolean)
     project_label = Column(String,ForeignKey('project.label'))
-        
+
     #many to one mapping to project
     project       = relationship("Project",backref=backref('nodes',order_by=id))
 
@@ -73,11 +73,11 @@ class Node(Model):
         self.available = available
 
     def __repr__(self):
-        return "Node<%r %r %r %r>"%(self.id, 
+        return "Node<%r %r %r %r>"%(self.id,
                                     self.label,
                                     self.available,
                                     self.project_label if self.project else None)
-    
+
 class Project(Model):
     id          = Column(Integer, primary_key = True)
     label       = Column(String)
@@ -103,7 +103,7 @@ class Vlan(Model):
     available     = Column(Boolean)
     nic_label     = Column(String)
     project_label = Column(String,ForeignKey('project.label'))
-    
+
     project         = relationship("Project",backref=backref('vlans',order_by=nic_label))
     def __init__(self,label, nic_label, available=True):
         self.label = label
@@ -150,7 +150,7 @@ class User(Model):
     id          = Column(Integer, primary_key = True)
     label       = Column(String)  #username
     hashed_password    = Column(String)
-    
+
     #many to many User<->Group
     """
     alice = User('alice', 'alice')
@@ -161,26 +161,26 @@ class User(Model):
     def __init__(self, label, password):
         self.label = label
         self.set_password(password)
-    
+
     def verify_password(self, password):
         return sha512_crypt.verify(password, self.hashed_password)
-    
+
     def set_password(self, password):
         self.hashed_password = sha512_crypt.encrypt(password)
-    
+
     def __repr__(self):
         return "User<%r %r %r %r>"%(self.id,
                                     self.label,
                                     self.hashed_password,
                                     self.groups)
-    
+
 class Group(Model):
     id            = Column(Integer, primary_key = True)
     label         = Column(String)
-    
+
     def __init__(self, label):
         self.label = label
-    
+
     def __repr__(self):
         return 'Group<%r %r>'%(self.id,
                                self.label)
@@ -189,7 +189,7 @@ class Headnode(Model):
     id            = Column(Integer, primary_key = True)
     label         = Column(String)
     available     = Column(Boolean)
-    
+
     project_label = Column(String, ForeignKey('project.label'))
     project       = relationship("Project", backref = backref('headnode',uselist = False))
 
