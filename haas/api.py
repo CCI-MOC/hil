3b218e4c8ec837ba6dd276cdd96de69b227617d5
George Silvis, III <george.iii.silvis@gmail.com>
2014-07-01 16:41:10 +0000

    Implement vlan_delete

--- haas/api.py
+++ haas/api.py
@@ -336,6 +336,25 @@ def vlan_register(vlan):
     db.add(model.Vlan(vlan_no))
     db.commit()
 
+def vlan_delete(vlan):
+    """Deletes the vlan with vlan number `vlan`.
+
+    Note that vlan should be a *string*, not a number. It is intended to be
+    pulled right from the HTTP request; this function will validate the
+    argument.
+    """
+    try:
+        vlan_no = int(vlan)
+    except Exception:
+        raise BadArgumentError('vlan:%s' % vlan)
+    if vlan_no < 1 or vlan_no > 4096:
+        raise BadArgumentError('vlan out of range: %d', vlan_no)
+
+    db = model.Session()
+    db.delete(_must_find(db, model.Vlan, str(vlan_no)))
+    db.commit()
+
+
     # Helper functions #
     ####################
 
