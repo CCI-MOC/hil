3c19234050cd11e9b9f25f4cfbc6f21aeee23b07
Ian Denhardt <ian@zenhack.net>
2014-07-07 10:37:36 -0400

    Added some `@no_dry_run` tests.
    This involved a bit of refactoring - I am of the opinion we should be
    doing more of this in our test suite; its easier to look at code like
    this and have a clear idea of what it's testing, particularly when we
    have a lot of tests.
    
    I ended up taking things out of the class, mostly because python's
    scoping rules are such that the test cases themselves would have had to
    have been (e.g.):
    
        def test_dry_run(self):
            TestNoDryRun._dry(TestNoDryRun._function)
    
    ...which hurts my eyes. In any case, for right now there aren't any
    other test classes in the file.

--- tests/unit/dev_support.py
+++ tests/unit/dev_support.py
@@ -1,23 +1,41 @@
 from haas.dev_support import no_dry_run
 from haas.config import cfg
+import pytest
 
 cfg.add_section('devel')
-cfg.set('devel', 'dry_run', True)
+
+# We test two ways of using the decorator: applying it to a freestanding
+# function, and applying it to an instance method.
+def _function():
+    @no_dry_run
+    def func():
+        assert False
+    func()
 
 
-class TestNoDryRun:
+def _method():
+    class Cls:
 
-    def test_function(self):
         @no_dry_run
-        def func():
+        def method(self):
             assert False
+    obj = Cls()
+    obj.method()
+
+
+# We test the decorator both with the option enabled and with it disabled.
+def _dry(func):
+    cfg.set('devel', 'dry_run', True)
+    func()
+
+
+def _wet(func):
+    cfg.remove_option('devel', 'dry_run')
+    with pytest.raises(AssertionError):
         func()
 
-    def test_method(self):
-        class Cls:
-
-            @no_dry_run
-            def method(self):
-                assert False
-        obj = Cls()
-        obj.method()
+# Actual test cases:
+def test_dry_function(): _dry(_function)
+def test_wet_function(): _wet(_function)
+def test_dry_method(): _dry(_method)
+def test_wet_method(): _wet(_method)
