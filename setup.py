940315e72aa344ae6732d64544a0ac70ff64da00
Ian Denhardt <ian@zenhack.net>
2015-02-23 01:50:46 -0500

    Move dependencies to setup.py.
    This addresses cci-moc/haas#396.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2014 Massachusetts Open Cloud Contributors
+# Copyright 2013-2015 Massachusetts Open Cloud Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the
@@ -13,13 +13,13 @@
 # governing permissions and limitations under the License.
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-
-requirements = [str(r.req) for r in parse_requirements('requirements.txt')]
 
 setup(name='moc-rest',
       version='0.1',
       url='https://github.com/CCI-MOC/moc-rest',
       packages=find_packages(),
-      install_requires=requirements,
-      )
+      install_requires=['Werkzeug==0.9.4',
+                        'pytest==2.6.2',
+                        'pytest-cov==1.8.0',
+                        'schema==0.3.1',
+                        ])
