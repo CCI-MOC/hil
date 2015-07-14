dfdc7f856cefcb0c5543e56c25e36832490abcdd
Ian Denhardt <ian@zenhack.net>
2015-07-14 18:04:14 -0400

    Port the nexus driver to the new driver model

--- haas/ext/switches/nexus.py
+++ haas/ext/switches/nexus.py
@@ -18,58 +18,190 @@ Currently the driver uses telnet to connect to the switch's console; in the
 long term we want to be using SNMP.
 """
 
-import os
 import pexpect
 import re
+import schema
+import logging
 
-from haas.dev_support import no_dry_run
+from sqlalchemy import Column, String, Integer, ForeignKey
 
-@no_dry_run
-def apply_networking(net_map, config):
-    def set_access_vlan(port, vlan_id):
-        """Set a port to access a given vlan.
+from haas.model import Switch
+from haas.ext.switches import _console
 
-        This function expects to be called while in the config prompt, and
-        leaves you there when done.
-        """
-        console.sendline('int ethernet 1/%s' % port)
-        # set it first to switch mode then to access mode:
-        console.expect(r'[\r\n]+.+# ')
-        console.sendline('sw')
-        console.expect(r'[\r\n]+.+# ')
-        console.sendline('sw mode access')
-        console.expect(r'[\r\n]+.+# ')
+logger = logging.getLogger(__name__)
 
-        if vlan_id is None:
-            # turn the port off
-            console.sendline('no sw')
-        else:
-            # set the vlan:
-            console.sendline('sw access vlan %s' % vlan_id)
-        console.expect(r'[\r\n]+.+# ')
-        # back out to config_prompt
-        console.sendline('exit')
-        console.expect(r'[\r\n]+.+# ')
 
-    nexus_ip = config['ip']
-    nexus_user = config['user']
-    nexus_pass = config['pass']
-    try:
-       console = pexpect.spawn('telnet ' + nexus_ip)
-       console.expect('login: ')
-       console.sendline(nexus_user)
-       console.expect('password: ')
-       console.sendline(nexus_pass)
-       console.expect(r'[\r\n]+.+# ')
-       console.sendline('config terminal')
-       console.expect(r'[\r\n]+.+# ')
+class Nexus(Switch):
+    api_name = 'http://schema.massopencloud.org/haas/switches/nexus'
 
-       for port_id in net_map:
-            set_access_vlan(port_id, net_map[port_id])
+    __mapper_args__ = {
+        'polymorphic_identity': api_name,
+    }
 
-    except IOError as e:
-        print "Connection error while connecting to the Nexus switch({0}): {1}".format(e.errno, e.strerror)
-    except:
-        print "Unexpected error while connecting to the Nexus switch:", sys.exc_info()[0]
-        raise
-        sys.Exit(1)
+    id = Column(Integer, ForeignKey('switch.id'), primary_key=True)
+    hostname = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    dummy_vlan = Column(String, nullable=False)
+
+    @staticmethod
+    def validate(kwargs):
+        schema.Schema({
+            'username': basestring,
+            'hostname': basestring,
+            'password': basestring,
+            'dummy_vlan': schema.And(schema.Use(int),
+                                     lambda v: 0 <= v and v <= 4096,
+                                     schema.Use(str)),
+        }).validate(kwargs)
+
+    def session(self):
+        return _Session.connect(self)
+
+
+class _Session(_console.Session):
+
+    def __init__(self, config_prompt, if_prompt, main_prompt, switch, console,
+                 dummy_vlan):
+        self.config_prompt = config_prompt
+        self.if_prompt     = if_prompt
+        self.main_prompt   = main_prompt
+        self.switch        = switch
+        self.console       = console
+        self.dummy_vlan    = dummy_vlan
+
+    def enter_if_prompt(self, interface):
+        self.console.sendline('config terminal')
+        self.console.sendline('int %s' % interface)
+
+    def exit_if_prompt(self):
+        self.console.sendline('exit')
+        self.console.sendline('exit')
+
+    def enable_vlan(self, vlan_id):
+        self.console.sendline('sw')
+        self.console.sendline('sw mode trunk')
+        self.console.sendline('sw trunk allowed vlan add %s' % vlan_id)
+
+    def disable_vlan(self, vlan_id):
+        self.console.sendline('sw trunk allowed vlan remove %s' % vlan_id)
+
+    def set_native(self, old, new):
+        if old is not None:
+            self.disable_vlan(old)
+        self.console.sendline('sw trunk native vlan %s' % new)
+        self.enable_vlan(new)
+
+    def disable_native(self, vlan_id):
+        self.disable_vlan(vlan_id)
+        self.console.sendline('sw trunk native vlan ' + self.dummy_vlan)
+
+    def disconnect(self):
+        self.console.sendline('exit')
+
+    @staticmethod
+    def connect(switch):
+        console = pexpect.spawn('telnet ' + switch.hostname)
+        console.expect('login: ')
+        console.sendline(switch.username)
+        console.expect('Password: ')
+        console.sendline(switch.password)
+
+        console = pexpect.spawn('telnet ' + switch.hostname)
+        console.expect('login: ')
+        console.sendline(switch.username)
+        console.expect('Password: ')
+        console.sendline(switch.password)
+
+        prompts = _console.get_prompts(console)
+
+        return _Session(console=console,
+                        dummy_vlan=switch.dummy_vlan,
+                        switch=switch,
+                        **prompts)
+
+    def _port_configs(self, ports):
+        alternatives = [
+            re.escape(r'--More--'),
+            r'Name:[^\n]*\n',
+            r'  [A-Z][^:]*:[^\n]*\n',
+            r'[\r\n]+.+# ',
+        ]
+        self.console.sendline('show int sw')
+
+        # Find the first interface name
+        self.console.expect(alternatives[1])
+
+        _, interface = self.console.after.split(':', 1)
+        interface = interface.strip()
+        info = {interface: {}}
+
+        while True:
+            index = self.console.expect(alternatives)
+            if index == 0:
+                self.console.send(' ')
+            elif index == 1:
+                _, interface = self.console.after.split(':', 1)
+                interface = interface.strip()
+                info[interface] = {}
+            elif index == 2:
+                k, v = self.console.after.split(':', 1)
+                info[interface][k.strip()] = v.strip()
+            elif index == 3:
+                break
+
+        # The output of show int sw calls things "EthernetX/YY", but
+        # everything else calls things "ethernet X/YY". Let's do the conversion
+        # here.
+        names_result = {}
+        pattern = re.compile(r'Ethernet(\d+)/(\d+)')
+        for k, v in info.iteritems():
+            match = re.match(pattern, k)
+            # TODO: We should verify there is in fact a match (there always
+            # should be, but let's be paranoid).
+            switch, port = match.groups()
+            names_result['ethernet %s/%s' % (switch, port)] = v
+
+        result = {}
+        for port in ports:
+            result[port] = names_result[port.label]
+
+        return result
+
+    def get_port_networks(self, ports):
+        num_re = re.compile(r'(\d+)')
+        port_configs = self._port_configs(ports)
+        result = {}
+
+        for k, v in port_configs.iteritems():
+            networks = []
+            if 'Trunking Native Mode VLAN' not in v:
+                # XXX (probable BUG): For some reason the last port on the switch
+                # sometimes isn't read correctly. For now just don't use that port
+                # for the test suite, and will skip it if this happens.
+                continue
+            native = v['Trunking Native Mode VLAN'].strip()
+            match = re.match(num_re, native)
+            if match:
+                num_str = match.groups()[0]
+                native = int(num_str)
+                if native == int(self.switch.dummy_vlan):
+                    native = None
+            else:
+                native = None
+            for range_str in v['Trunking VLANs Allowed'].split(','):
+                # XXX TODO make this actualy interpret e.g. 2-7 as a *range*
+                for num_str in range_str.split('-'):
+                    num_str = num_str.strip()
+                    match = re.match(num_re, num_str)
+                    if match:
+                        # There may be other tokens in the output, e.g.
+                        # the string "(Inactive)" somteimtes appears.
+                        # We should only use the value if it's an actual number.
+                        num_str = match.groups()[0]
+                        networks.append(('vlan/%s' % num_str, int(num_str)))
+
+            if native is not None:
+                networks.append(('vlan/native', native))
+            result[k] = networks
+        return result
