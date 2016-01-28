0096450168da0b3391c7fb733748ae92c6289212
Ian Denhardt <ian@zenhack.net>
2016-01-27 20:13:14 -0500

    Generalize auth test function for use with other api calls.
    We'll start adding tests for other functions shortly.

--- tests/unit/api/auth.py
+++ tests/unit/api/auth.py
@@ -46,65 +46,79 @@ pytestmark = pytest.mark.usefixtures('configure',
                                      'with_request_context')
 
 
-@pytest.mark.parametrize('error,admin,project,creator,access,net_id', [
+@pytest.mark.parametrize('fn,error,admin,project,args', [
     # TODO: Find out if there's a way to pass these by kwargs; it would be more
-    # readable. For now, we try to make things a little better by putting all
-    # of the actual arguments to network_create on the second line of each
-    # entry.
+    # readable. For now, we try to make things a little better by formatting
+    # each entry as:
+    #
+    # (fn, error,
+    #  admin, project,
+    #  args),
 
-    # Legal cases:
+    # network_create
 
-    ## Admin creates a public network internal to HaaS:
-    (None, True, None,
-     'admin', '', ''),
+    ### Legal cases:
 
-    ## Admin creates a public network with an existing net_id:
-    (None, True, None,
-     'admin', '', 'some-id'),
+    ### Admin creates a public network internal to HaaS:
+    (api.network_create, None,
+     True, None,
+     ['pub', 'admin', '', '']),
 
-    ## Admin creates a provider network for some project:
-    (None, True, None,
-     'admin', 'runway', 'some-id'),
+    ### Admin creates a public network with an existing net_id:
+    (api.network_create, None,
+     True, None,
+     ['pub', 'admin', '', 'some-id']),
 
-    ## Admin creates an allocated network on behalf of a project. Silly, but
-    ## legal.
-    (None, True, None,
-     'admin', 'runway', ''),
+    ### Admin creates a provider network for some project:
+    (api.network_create, None,
+     True, None,
+     ['pxe', 'admin', 'runway', 'some-id']),
 
-    ## Project creates a private network for themselves:
-    (None, False, 'runway',
-     'runway', 'runway', ''),
+    ### Admin creates an allocated network on behalf of a project. Silly, but
+    ### legal.
+    (api.network_create, None,
+     True, None,
+     ['pxe', 'admin', 'runway', '']),
 
-    # Illegal cases:
+    ### Project creates a private network for themselves:
+    (api.network_create, None,
+     False, 'runway',
+     ['pxe', 'runway', 'runway', '']),
 
-    ## Project tries to create a private network for another project:
-    (AuthorizationError, False, 'runway',
-     'manhattan', 'manhattan', ''),
+    ## Illegal cases:
 
-    ## Project tries to specify a net_id:
-    (BadArgumentError, False, 'runway',
-     'runway', 'runway', 'some-id'),
+    ### Project tries to create a private network for another project.
+    (api.network_create, AuthorizationError,
+     False, 'runway',
+     ['pxe', 'manhattan', 'manhattan', '']),
 
-    ## Project tries to create a public network:
-    (AuthorizationError, False, 'runway',
-     'admin', '', ''),
+    ### Project tries to specify a net_id:
+    (api.network_create, BadArgumentError,
+     False, 'runway',
+     ['pxe', 'runway', 'runway', 'some-id']),
 
-    ## Project tries to set creator to 'admin' on its own network:
-    (AuthorizationError, False, 'runway',
-     'admin', 'runway', ''),
+    ### Project tries to create a public network:
+    (api.network_create, AuthorizationError,
+     False, 'runway',
+     ['pub', 'admin', '', '']),
+
+    ### Project tries to set creator to 'admin' on its own network:
+    (api.network_create, AuthorizationError,
+     False, 'runway',
+     ['pxe', 'admin', 'runway', '']),
 ])
-def test_network_create(error, admin, project, creator, access, net_id):
-    """Test network_create with various arguments/authentication.
+def test_auth_call(fn, error, admin, project, args):
+    """Test the authorization properties of an api call.
 
     Parmeters:
 
-        * `auth_backend` - the auth backend. provided by the fixture.
+        * `fn` - the api function to call
         * `error` - The error that should be raised. None if no error should
                     be raised.
         * `admin` - Whether the request should have admin access.
-        * `project` - The project the request should be authenticated as. Can
-                      be None if `admin` is True.
-        * `creator`, `access`, and `net_id` - The arguments to network_create.
+        * `project` - The name of the project the request should be
+                      authenticated as. Can be None if `admin` is True.
+        * `args` - the arguments (as a list) to `fn`.
     """
     auth_backend = get_auth_backend()
     auth_backend.set_admin(admin)
@@ -112,11 +126,8 @@ def test_network_create(error, admin, project, creator, access, net_id):
         project = local.db.query(model.Project).filter_by(label=project).one()
         auth_backend.set_project(project)
 
-    def call():
-        api.network_create('pxe', creator, access, net_id)
-
     if error is None:
-        call()
+        fn(*args)
     else:
         with pytest.raises(error):
-            call()
+            fn(*args)
