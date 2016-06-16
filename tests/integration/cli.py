a83e3a86a6a82daaa6d4701bb7acb9a3a831480d
Kristi Nikolla <knikolla@bu.edu>
2016-06-16 10:16:50 -0400

    CI: Added ext.obm.mock to config and assertion to list_projects

--- tests/integration/cli.py
+++ tests/integration/cli.py
@@ -2,23 +2,29 @@ import json
 import subprocess
 
 PROJECT1 = 'foo'
+PROJECT2 = 'bar'
 
 
 def test_add_list_delete_projects():
-    """ Test the CRUD operations on projects from the CLI """
+    """ Test the CRUD operations on projects from the CLI.
+
+    The project list does not need to be empty before running the test."""
     output = subprocess.check_output(['haas', 'list_projects'])
     projects = json.loads(output)
     assert PROJECT1 not in projects
+    assert PROJECT2 not in projects
     size = len(projects)
 
     subprocess.check_call(['haas', 'project_create', PROJECT1])
     output = subprocess.check_output(['haas', 'list_projects'])
     projects = json.loads(output)
     assert PROJECT1 in projects
+    assert PROJECT2 not in projects
     assert len(projects) == size + 1
 
     subprocess.check_call(['haas', 'project_delete', PROJECT1])
     output = subprocess.check_output(['haas', 'list_projects'])
     projects = json.loads(output)
     assert PROJECT1 not in projects
+    assert PROJECT2 not in projects
     assert len(projects) == size
