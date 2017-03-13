a371449d87f29e8feaa0ec6e1c03dbc3862896a6
Naved Ansari <naved001@hack-n-hil.prb.massopencloud.org>
2017-03-13 14:55:26 -0400

    dell driver now supports saving running-config

--- haas/ext/switches/dell.py
+++ haas/ext/switches/dell.py
@@ -26,6 +26,8 @@ from haas.model import db, Switch
 from haas.migrations import paths
 from haas.ext.switches import _console
 from os.path import dirname, join
+from haas.config import cfg
+import time
 
 paths[__name__] = join(dirname(__file__), 'migrations', 'dell')
 
@@ -94,6 +96,8 @@ class _Session(_console.Session):
     def exit_if_prompt(self):
         self._sendline('exit')
         self._sendline('exit')
+        if cfg.get('switch_mode', 'mode') == 'ALWAYS':
+                    self._save_running_config()
 
     def enable_vlan(self, vlan_id):
         self._sendline('sw mode trunk')
@@ -113,6 +117,8 @@ class _Session(_console.Session):
         self._sendline('sw trunk native vlan none')
 
     def disconnect(self):
+        if cfg.get('switch_mode', 'mode') == 'DISCONNECT':
+            self._save_running_config()
         self._sendline('exit')
         self.console.expect(pexpect.EOF)
         logger.debug('Logged out of switch %r', self.switch)
@@ -187,3 +193,11 @@ class _Session(_console.Session):
 
         self.console.expect(self.main_prompt)
         return result
+
+    def _save_running_config(self):
+        """saves the running config to startup config"""
+
+        self._sendline('copy running-config startup-config')
+        time.sleep(1)
+        self._sendline('y')
+        time.sleep(7)
