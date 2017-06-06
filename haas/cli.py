a6024ebc02281a032d358562f854cb4df60b127f
Izhmash <ianballou67@gmail.com>
2017-06-06 15:13:49 -0400

    included type-specific except blocks

--- haas/cli.py
+++ haas/cli.py
@@ -815,9 +815,9 @@ def main():
         setup_http_client()
         try:
             command_dict[sys.argv[1]](*sys.argv[2:])
+        except FailedAPICallException
+            sys.exit(1)
+        except InvalidAPIArgumentsException:
+            sys.exit(2)
         except Exception as e:
             sys.exit('Error: %s\n' % e.message)
-        #except FailedAPICallException
-        #    sys.exit(1)
-        #except InvalidAPIArgumentsException:
-        #    sys.exit(2)
