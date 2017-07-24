a4292e5afd89a9c75f6cd87415a937c99968d2c3
Izhmash <ianballou67@gmail.com>
2017-07-24 15:50:37 -0400

    mock switch migration added

---
+++ hil/ext/switches/migrations/mock/fa9ef2c9b67f_mock_switch_pk_to_bigint.py
@@ -0,0 +1,28 @@
+"""mock switch PK to bigint
+
+Revision ID: fa9ef2c9b67f
+Revises: b5b31d19257d
+Create Date: 2017-07-24 15:46:29.332253
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa9ef2c9b67f'
+down_revision = 'b5b31d19257d'
+branch_labels = ('hil.ext.switches.mock',)
+
+
+def upgrade():
+    op.alter_column('mock_switch', 'id',
+                    existing_type=sa.Integer(),
+                    type_=sa.BIGINT())
+
+
+def downgrade():
+    op.alter_column('mock_switch', 'id',
+                    existing_type=sa.BIGINT(),
+                    type_=sa.Integer())
