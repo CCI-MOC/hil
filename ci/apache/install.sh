bbfb3c561d3b55b49c31c4781f8231243324e469
Ian Denhardt <ian@zenhack.net>
2017-10-02 16:11:36 -0400

    apache CI: run sed command with sudo

--- ci/apache/install.sh
+++ ci/apache/install.sh
@@ -26,7 +26,7 @@ sudo chown -R travis:travis /var/www
 mkdir /var/www/hil
 
 sudo cp ci/wsgi.conf /etc/apache2/sites-available/hil.conf
-sed -e "s|%VIRTUAL_ENV%|$VIRTUAL_ENV|g" -i /etc/apache2/sites-available/hil.conf
+sudo sed -e "s|%VIRTUAL_ENV%|$VIRTUAL_ENV|g" -i /etc/apache2/sites-available/hil.conf
 cp hil.wsgi /var/www/hil/hil.wsgi
 
 sudo a2dissite 000-default && sudo a2ensite hil
