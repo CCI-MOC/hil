4be21e0ecf5a469ef969fc2d3745dcdf7b8ae716
Ian Denhardt <ian@zenhack.net>
2017-10-02 16:05:21 -0400

    Only run the trailing whitespace check with SQLite.

--- ci/no_trailing_whitespace.sh
+++ ci/no_trailing_whitespace.sh
@@ -2,6 +2,12 @@
 # failing if it finds any. Note that (a) it removes the whitespace as a side
 # effect, and (b) it will fail unless HEAD is trailing-whitespace free, even
 # if the working directory is OK.
+
+if [ "$DB" != sqlite ] ; then
+	# Only run in the SQLite pass.
+	exit 0
+fi
+
 cd "$(git rev-parse --show-toplevel)" # Enter the source tree root
 
 # Remove all trailing whitespace in tracked files:
