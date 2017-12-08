ef3da6a0609d834db77050a8ae4bfc4be96efa2a
Ian Ballou <ianballou67@gmail.com>
2017-12-08 16:45:46 -0500

    First pass at checking for URL reserved characters in switch.py

--- hil/client/switch.py
+++ hil/client/switch.py
@@ -1,6 +1,7 @@
 """Client support for switch related api calls."""
 import json
 from hil.client.base import ClientBase
+from hil.errors import BadArgumentError
 
 
 class Switch(ClientBase):
@@ -26,11 +27,19 @@ class Switch(ClientBase):
 
     def delete(self, switch):
         """Deletes the switch named <switch>."""
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch)
         return self.check_response(self.httpClient.request("DELETE", url))
 
     def show(self, switch):
         """Shows attributes of <switch>. """
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch)
         return self.check_response(self.httpClient.request("GET", url))
 
@@ -40,16 +49,40 @@ class Port(ClientBase):
 
     def register(self, switch, port):
         """Register a <port> with <switch>. """
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
+        bad_chars = self.find_reserved_w_slash(port)
+        if bool(bad_chars):
+            raise BadArgumentError("Ports may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch, 'port', port)
         return self.check_response(self.httpClient.request("PUT", url))
 
     def delete(self, switch, port):
         """Deletes information of the <port> for <switch> """
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
+        bad_chars = self.find_reserved_w_slash(port)
+        if bool(bad_chars):
+            raise BadArgumentError("Ports may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch, 'port', port)
         return self.check_response(self.httpClient.request("DELETE", url))
 
     def connect_nic(self, switch, port, node, nic):
         """Connects <port> of <switch> to <nic> of <node>. """
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
+        bad_chars = self.find_reserved_w_slash(port)
+        if bool(bad_chars):
+            raise BadArgumentError("Ports may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch, 'port', port, 'connect_nic')
         payload = json.dumps({'node': node, 'nic': nic})
         return self.check_response(
@@ -58,15 +91,39 @@ class Port(ClientBase):
 
     def detach_nic(self, switch, port):
         """"Detaches <port> of <switch>. """
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
+        bad_chars = self.find_reserved_w_slash(port)
+        if bool(bad_chars):
+            raise BadArgumentError("Ports may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch, 'port', port, 'detach_nic')
         return self.check_response(self.httpClient.request("POST", url))
 
     def show(self, switch, port):
         """Show what's connected to <port>"""
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
+        bad_chars = self.find_reserved_w_slash(port)
+        if bool(bad_chars):
+            raise BadArgumentError("Ports may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch, 'port', port)
         return self.check_response(self.httpClient.request("GET", url))
 
     def port_revert(self, switch, port):
         """removes all vlans from a switch port"""
+        bad_chars = self.find_reserved(switch)
+        if bool(bad_chars):
+            raise BadArgumentError("Switches may not contain: %s"
+                                   % bad_chars)
+        bad_chars = self.find_reserved_w_slash(port)
+        if bool(bad_chars):
+            raise BadArgumentError("Ports may not contain: %s"
+                                   % bad_chars)
         url = self.object_url('switch', switch, 'port', port, 'revert')
         return self.check_response(self.httpClient.request("POST", url))
