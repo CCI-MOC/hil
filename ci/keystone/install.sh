b142f7b178ce7830e992a9254c31ed3ed0bbe5af
naved001 <naved001@gmail.com>
2017-12-18 17:10:38 -0500

    Remove incorrect package
    * We wanted to install keystone-client from `extras` in `setup.py` but there's
    a typo here. I removed the pacakge altogether because it is also installed by
    keystonemiddleware; which makes installing keystone-client redundant.

--- ci/keystone/install.sh
+++ ci/keystone/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -ex
 
-pip install .[keystone-auth-backend,keystone-cli]
+pip install .[keystone-auth-backend]
 
 # The exact commit we use here is somewhat arbitrary, but we want
 # something that (a) won't change out from under our feet, and (b)
