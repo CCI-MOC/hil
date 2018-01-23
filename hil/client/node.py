1d1b5fd66b035790c6cca466d7f306543ad83455
Lucas H. Xu <xuh@bu.edu>
2018-01-23 15:10:45 -0500

    change the hardcoded obm_types

--- hil/client/node.py
+++ hil/client/node.py
@@ -33,21 +33,18 @@ class Node(ClientBase):
         # obm_api = "http://schema.massopencloud.org/haas/v0/obm/"
         # obm_types = ["ipmi", "mock"]
         obm_api = "http://schema.massopencloud.org/haas/v0/obm/"
-        obm_types = ["ipmi", "mock"]
+        ext_url = self.object_url('active_extensions')
+        ext_resp = self.check_response(self.httpClient.request("GET", ext_url))
+        obm_types = []
+        for resp in ext_resp:
+            if "hil.ext.obm" in resp:
+                obm_types.append(resp.split(".")[-1])
+
         if subtype in obm_types:
             if len(args) == 3:
                 obminfo = {"type": obm_api + subtype, "host": args[0],
                            "user": args[1], "password": args[2]
                            }
-            else:
-                sys.stderr.write('ERROR: subtype ' + subtype +
-                                 ' requires exactly 3 arguments\n')
-                sys.stderr.write('<hostname> <ipmi-username> <ipmi-password>\n')
-                return
-        else:
-            sys.stderr.write('ERROR: Wrong OBM subtype supplied\n')
-            sys.stderr.write('Supported OBM sub-types: ipmi, mock\n')
-            return
         url = self.object_url('node', node)
         payload = json.dumps({"obm": obminfo})
 	return self.check_response(
