c5486be67fd7c775df6ec9297034232068133807
Lucas H. Xu <xuh@bu.edu>
2018-01-23 11:30:56 -0500

    fix E128/E501

--- hil/api.py
+++ hil/api.py
@@ -697,14 +697,16 @@ def list_networks():
         for n in networks:
             if n.access:
                 result[n.label] = {'network_id': n.network_id,
-                       'projects': sorted([p.label for p in n.access])}
+                                   'projects': sorted([p.label for p in n.access])}
             else:
-                result[n.label] = {'network_id': n.network_id, 'projects': None}
+                result[n.label] = {'network_id': n.network_id, 
+                                   'projects': None}
     # Regular User Operation
     else:
         pub_nets = db.session.query(model.Network).filter_by(access=None).all()
         for n in pub_nets:
-            result[n.label] = {'network_id': n.network_id, 'projects': None}
+            result[n.label] = {'network_id': n.network_id, 
+                               'projects': None}
 
     return json.dumps(result, sort_keys=True)
 
