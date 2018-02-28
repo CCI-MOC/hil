e41dc6309202446bc14c5a3f35b974d422ada384
naved001 <naved001@gmail.com>
2018-02-28 13:05:51 -0500

    Import extensions in tests
    * remove unused variable.

--- tests/unit/ext/switches/common.py
+++ tests/unit/ext/switches/common.py
@@ -1,10 +1,9 @@
+"""Unit tests for hil.ext.switches.common"""
+
 import pytest
 
 from hil import config
-from hil.ext.switches.common import parse_vlans, should_save
 from hil.test_common import config_testsuite, config_merge
-from hil.ext.switches.brocade import Brocade
-from hil.ext.switches.dell import PowerConnect55xx
 
 
 @pytest.fixture
@@ -31,7 +30,9 @@ def configure():
 
 def test_parse_vlans():
     """Test parse_vlans"""
-    sample_inputs = ['12,14-18,23,28,80-90', '20', '20,22', '20-22']
+    # Have to import the method here, otherwise anything starting with
+    # "hil.ext" pollutes the env.
+    from hil.ext.switches.common import parse_vlans
     assert parse_vlans('12,14') == ['12', '14']
     assert parse_vlans('20-22') == ['20', '21', '22']
     assert parse_vlans('1512') == ['1512']
@@ -41,6 +42,10 @@ def test_parse_vlans():
 
 def test_should_save(configure):
     """Test should save method"""
+    from hil.ext.switches.brocade import Brocade
+    from hil.ext.switches.dell import PowerConnect55xx
+    from hil.ext.switches.common import should_save
+
     brocade = Brocade()
     dell = PowerConnect55xx()
 
