f867cdbfd7eb4842a09414b03278824b2d40ec08
naved001 <naved001@gmail.com>
2018-03-07 16:21:56 -0500

    Add project subcommands
    * Remove duplicate project group as pointed out by @xuhang57
    * add the node delete command

--- hil/cli.py
+++ hil/cli.py
@@ -115,10 +115,6 @@ def project():
     """Commands related to project"""
 
 @cli.group()
-def project():
-    """Commands related to project"""
-
-@cli.group()
 def network():
     """Commands related to network"""
 
@@ -139,7 +135,7 @@ def user():
 ######################################
 
 @cli.command(name='serve', help='Run a development HIL server')
-@click.argument('port', type=click.Choice(str(i) for i in range(1,2**16)),
+@click.argument('port', type=click.IntRange(0,2**16-1),
                 default='5000', required=False)
 def serve(port):
     """Run a development api server. Don't use this in production."""
@@ -245,6 +241,12 @@ def node_register(node, obmtype, hostname, username, password):
     """
     C.node.register(node, obmtype, hostname, username, password)
 
+@node.command(name='delete', help='Delete a node')
+@click.argument('node')
+def node_delete(node):
+    """Delete <node>"""
+    C.node.delete(node)
+
 
 ###########################
 # node connect/detach network implementations. Could also be expanded to do
@@ -278,7 +280,7 @@ def node_detach_network(node, network, nic):
     print C.node.detach_network(node, nic, network)
 
 ###########################
-
+# I could also implement this the way I am doing list project <return or object>
 @node.group(name='nic', help='Node nic commands')
 def node_nic():
     """Connect node an <object>"""
@@ -371,10 +373,79 @@ def node_stop_console(node):
     C.node.stop_console(node)
 
 
-
 ####################################
 ### PROJECT SUBCOMMANDS GO HERE! ###
 ####################################
+
+###########################
+# I am invoking the project list and project list <something> commands here
+# one downside of this is that the command `hil project list` will just
+# complete without telling that there's more to that command. However,
+# I have written in the help, that it can also invoke a subcommand.
+# Alternatively, just as other subcommands, one can type
+# `hil project list --help` to see what more can be done.
+
+@project.group(name='list', invoke_without_command=True)
+@click.pass_context
+def project_list(ctx):
+    """List all projects or invoke subcommand
+    """
+    if ctx.invoked_subcommand is None:
+        """List all projects"""
+        q = C.project.list()
+        sys.stdout.write('%s Projects :    ' % len(q) + " ".join(q) + '\n')
+
+@project_list.command(name='networks', help='List all networks that belong to a project')
+@click.argument('project')
+def project_list_networks(project):
+    print "Project list networks "
+
+@project_list.command(name='nodes', help='List all nodes that belong to a project')
+@click.argument('project')
+def project_list_nodes(project):
+    """List all nodes attached to a <project>"""
+    q = C.project.nodes_in(project)
+    sys.stdout.write('Nodes allocated to %s:  ' % project + " ".join(q) + '\n')
+
+###########################
+
+@project.command(name='create')
+@click.argument('project')
+def project_create(project):
+    """Create a new project"""
+    C.project.create(project)
+
+@project.command(name='delete')
+@click.argument('project')
+def project_delete(project):
+    """Delete a project"""
+    C.project.delete(project)
+
+###########################
+@project.group(name='connect')
+def project_connect():
+    """Connect project to an <object>"""
+
+@project_connect.command(name='node')
+@click.argument('project')
+@click.argument('node')
+def project_connect_node(project, node):
+    """Connect <node> to <project>"""
+    C.project.connect(project, node)
+
+@project.group(name='detach')
+def project_detach():
+    """Detach a project from an <object>"""
+
+@project_detach.command(name='node')
+@click.argument('project')
+@click.argument('node')
+def project_detach_node(project, node):
+    """Detach <node> from <project>"""
+    C.project.detach(project, node)
+
+###########################
+
 
 
 def main():
