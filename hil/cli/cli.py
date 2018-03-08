edf5f7caca47bccbadf62d234414ae1075b3781b
naved001 <naved001@gmail.com>
2018-03-08 13:09:17 -0500

    Remove unnecessary error handling
    * Add the version command

--- hil/cli/cli.py
+++ hil/cli/cli.py
@@ -3,16 +3,13 @@ import sys
 import pkg_resources
 
 from hil.client.base import FailedAPICallException
-from hil.errors import BadArgumentError
 from hil.commands.util import ensure_not_root
 from hil.cli import node, project, network, switch, port, user, misc
 
 VERSION = pkg_resources.require('hil')[0].version
 
-class InvalidAPIArgumentsException(Exception):
-    """Exception indicating that the user passed invalid arguments."""
-
 @click.group()
+@click.version_option(version=VERSION)
 def cli():
     """The HIL Command line"""
 
@@ -22,15 +19,12 @@ commands = [node.node, project.project, network.network, switch.switch,
 for command in commands:
     cli.add_command(command)
 
+
 def main():
     ensure_not_root()
     try:
         cli()
     except FailedAPICallException as e:
-        sys.exit('Error: %s\n' % e.message)
-    except InvalidAPIArgumentsException as e:
-        sys.exit('Error: %s\n' % e.message)
-    except BadArgumentError as e:
         sys.exit('Error: %s\n' % e.message)
     except Exception as e:
         sys.exit('Unexpected error: %s\n' % e.message)
