d87b1875f679dfad01ae2575fae75575a83f754b
naved001 <naved001@gmail.com>
2018-03-08 17:05:29 -0500

    Cleanup the network subcommands
    * Implement `hil network project <action>` group.

--- hil/cli/network.py
+++ hil/cli/network.py
@@ -10,11 +10,8 @@ C = setup_http_client()
 def network():
     """Commands related to network"""
 
-####################################
-### NETWORK SUBCOMMANDS GO HERE! ###
-####################################
 
-@network.command(name='create')
+@network.command(name='create', short_help='Create a new network')
 @click.argument('network')
 @click.argument('owner')
 @click.option('--access')
@@ -31,58 +28,54 @@ def network_create(network, owner, access, net_id):
 @network.command(name='delete')
 @click.argument('network')
 def network_delete(network):
-    """Delete a <network>"""
+    """Delete a network"""
     C.network.delete(network)
 
 
 @network.command(name='show')
 @click.argument('network')
 def network_show(network):
-    """Display information about <network>"""
+    """Display information about network"""
     q = C.network.show(network)
     for item in q.items():
         sys.stdout.write("%s\t  :  %s\n" % (item[0], item[1]))
 
 
-###########################
-@network.group(name='list', invoke_without_command=True)
-@click.pass_context
-def network_list(ctx):
-    """List all networks or invoke a subcommand"""
-    if ctx.invoked_subcommand is None:
-        q = C.network.list()
-        for item in q.items():
-            sys.stdout.write('%s \t : %s\n' % (item[0], item[1]))
+@network.command(name='list')
+def network_list():
+    """List all networks"""
+    q = C.network.list()
+    for item in q.items():
+        sys.stdout.write('%s \t : %s\n' % (item[0], item[1]))
 
-@network_list.command('attachments')
+
+@network.group(name='project')
+def network_project():
+    """Commands that manipulate project and network relations"""
+
+
+@network_project.command('list')
 @click.argument('network')
-@click.argument('project')
+@click.option('--project')
 def list_network_attachments(network, project):
-    """List nodes connected to a network
-    <project> may be either "all" or a specific project name.
+    """Lists all the attachments from <project> for <network>
+
+    If <project> is `None`, lists all attachments for <network>
     """
     print C.network.list_network_attachments(network, project)
-###########################
-
-@network.group(name='grant-access')
-def network_grant_access():
-    """Grant network access to a project"""
 
 
-@network.group(name='revoke-access')
-def network_revoke_access():
-    """Grant network access to a project"""
-
-@network_grant_access.command(name='project')
-@click.argument('project')
+@network_project.command(name='add')
 @click.argument('network')
+@click.argument('project')
 def network_grant_project_access(project, network):
     """Add <project> to <network> access"""
     C.network.grant_access(project, network)
 
-@network_revoke_access.command(name='project')
-@click.argument('project')
+
+@network_project.command(name='remove')
 @click.argument('network')
+@click.argument('project')
 def network_revoke_project_access(project, network):
     """Remove <project> from <network> access"""
     C.network.revoke_access(project, network)
