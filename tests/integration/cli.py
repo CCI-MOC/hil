d9f8b8f110d521945ec17ee3167a6e2a39ee7dae
naved001 <naved001@gmail.com>
2018-03-08 17:58:10 -0500

    Fix CLI tests

--- tests/integration/cli.py
+++ tests/integration/cli.py
@@ -15,23 +15,23 @@ def test_add_list_delete_projects():
     """ Test the CRUD operations on projects from the CLI.
 
     The project list does not need to be empty before running the test."""
-    output = subprocess.check_output(['hil', 'list_projects'])
+    output = subprocess.check_output(['hil', 'project', 'list'])
     projects = output.split(" ")
     projects.remove('\n')
     assert PROJECT1 not in projects
     assert PROJECT2 not in projects
     size = len(projects)
 
-    subprocess.check_call(['hil', 'project_create', PROJECT1])
-    output = subprocess.check_output(['hil', 'list_projects'])
+    subprocess.check_call(['hil', 'project', 'create', PROJECT1])
+    output = subprocess.check_output(['hil', 'project', 'list'])
     output = output.strip('\n')
     projects = output.split(" ")
     assert PROJECT1 in projects
     assert PROJECT2 not in projects
     assert len(projects) == size + 1
 
-    subprocess.check_call(['hil', 'project_delete', PROJECT1])
-    output = subprocess.check_output(['hil', 'list_projects'])
+    subprocess.check_call(['hil', 'project', 'delete', PROJECT1])
+    output = subprocess.check_output(['hil', 'project', 'list'])
     projects = output.split(" ")
     projects.remove('\n')
     assert PROJECT1 not in projects
