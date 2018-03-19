01afa566d761b289304e95d81c692e6b1194f70a
naved001 <naved001@gmail.com>
2018-03-19 15:14:07 -0400

    Dont call client library's check response.
    * For show_console, we just return whatever is in the reponse rather than
    calling check_response (which performs a json.loads).

--- hil/client/node.py
+++ hil/client/node.py
@@ -1,6 +1,6 @@
 """Client support for node related api calls."""
 import json
-from hil.client.base import ClientBase
+from hil.client.base import ClientBase, FailedAPICallException
 from hil.client.base import check_reserved_chars
 from hil.errors import BadArgumentError, UnknownSubtypeError
 
@@ -145,7 +145,13 @@ class Node(ClientBase):
     def show_console(self, node):
         """Display console log for <node> """
         url = self.object_url('node', node, 'console')
-        return self.check_response(self.httpClient.request('GET', url))
+        response = self.httpClient.request('GET', url)
+        # we don't call check_response here because we want to return the
+        # raw byte stream, rather than converting it to json.
+        if 200 <= response.status_code < 300:
+            return response.content
+        raise FailedAPICallException(error_type=response.status_code,
+                                     message=response.content)
 
     @check_reserved_chars()
     def start_console(self, node):
