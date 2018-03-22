a8d401720ed9e94a62e75e1ed6dfbd71bbd956fb
Naved Ansari <naved001@gmail.com>
2018-03-22 15:48:17 -0400

    Switch register now uses the client library call

--- hil/cli/switch.py
+++ hil/cli/switch.py
@@ -1,6 +1,7 @@
 """Commands related to switch are in this module"""
 import click
 import sys
+import ast
 from hil.cli.client_setup import setup_http_client
 
 C = None
@@ -29,17 +30,72 @@ def list_switches():
     sys.stdout.write('%s switches :    ' % len(q) + " ".join(q) + '\n')
 
 
-@switch.command(name='register', help='Register a switch')
+@switch.command(name='register', short_help='Register a switch')
 @click.argument('switch')
 @click.argument('subtype')
 @click.argument('args', nargs=-1)
 def switch_register(switch, subtype, args):
-    """Register a switch with name <switch> and <subtype> <args>
+    """Register a switch with name <switch> and
+    <subtype>, <hostname>, <username>,  <password>
+
     eg. hil switch_register mock03 mock mockhost01 mockuser01 mockpass01
+
+    To make a generic switch register call, pass the arguments like:
+
+    hil switch_register switchname subtype switchinfo
+    hil switch_register dell-1 'http://example.com/schema/url' '{"foo": "bar"}'
     """
-    # hopefullt the switch_register client call will be done before the cli.the
-    # args is a tuple that can have unlimited number of arguments.
-    pass
+    switch_api = "http://schema.massopencloud.org/haas/v0/switches/"
+
+    if subtype == "nexus" or subtype == "delln3000":
+        if len(args) == 4:
+            switchinfo = {
+                "hostname": args[0],
+                "username": args[1],
+                "password": args[2],
+                "dummy_vlan": args[3]}
+            subtype = switch_api + subtype
+        else:
+            sys.exit('ERROR: subtype ' + subtype +
+                     ' requires exactly 4 arguments\n'
+                     '<hostname> <username> <password>'
+                     '<dummy_vlan_no>\n')
+
+    elif subtype == "mock" or subtype == "powerconnect55xx":
+        if len(args) == 3:
+            switchinfo = {"hostname": args[0],
+                          "username": args[1], "password": args[2]}
+            subtype = switch_api + subtype
+        else:
+            sys.exit('ERROR: subtype ' + subtype +
+                     ' requires exactly 3 arguments\n'
+                     '<hostname> <username> <password>\n')
+
+    elif subtype == "brocade" or subtype == "dellnos9":
+        if len(args) == 4:
+            switchinfo = {"hostname": args[0],
+                          "username": args[1], "password": args[2],
+                          "interface_type": args[3]}
+            subtype = switch_api + subtype
+        else:
+            sys.exit('ERROR: subtype ' + subtype +
+                     ' requires exactly 4 arguments\n'
+                     '<hostname> <username> <password> '
+                     '<interface_type>\n'
+                     'NOTE: interface_type refers '
+                     'to the speed of the switchports\n '
+                     'ex. TenGigabitEthernet, FortyGigabitEthernet, '
+                     'etc.\n')
+
+    else:
+        if len(args) == 0:
+            sys.exit('No switchinfo provided')
+        try:
+            switchinfo = ast.literal_eval(args[0])
+        except (ValueError, SyntaxError):
+            sys.exit('Malformed switchinfo')
+
+    C.switch.register(switch, subtype, switchinfo)
 
 
 @switch.command(name='delete', help='Delete a switch')
