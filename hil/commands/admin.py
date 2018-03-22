084bf875b403580b050a0b76916599e2108b8eb0
Lucas H. Xu <xuh@bu.edu>
2018-03-22 14:03:43 +0000

    Use flask_script since flask.ext.script is deprecated (in admin.py)

--- hil/commands/admin.py
+++ hil/commands/admin.py
@@ -4,7 +4,7 @@ from hil.commands import db
 from hil.commands.migrate_ipmi_info import MigrateIpmiInfo
 from hil.commands.util import ensure_not_root
 from hil.flaskapp import app
-from flask.ext.script import Manager
+from flask_script import Manager
 
 manager = Manager(app)
 manager.add_command('db', db.command)
