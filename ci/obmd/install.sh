95a7a5c945100aad689e7cffbf834bac59478125
Ian Denhardt <ian@zenhack.net>
2018-03-28 17:56:12 -0400

    Add -y option to apt-add-repository

--- ci/obmd/install.sh
+++ ci/obmd/install.sh
@@ -5,6 +5,6 @@ set -ex
 # a newer one from PPA. See also:
 #
 # https://github.com/golang/go/wiki/Ubuntu
-sudo add-apt-repository ppa:gophers/archive
+sudo add-apt-repository -y ppa:gophers/archive
 sudo apt-get update
 sudo apt-get -y install golang-1.10-go
