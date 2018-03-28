09b7e101cd1a9d6836218e062a3286c510aea1d0
Ian Denhardt <ian@zenhack.net>
2018-03-28 17:49:20 -0400

    Install Go 1.10 from PPA.

--- ci/obmd/run_integration_tests.sh
+++ ci/obmd/run_integration_tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 set -ex
 
+# The PPA version of Go stores its executables outside of the normal path, so
+# we have to set this, otherwise we'll end up using Go 1.7.
+export PATH="/usr/lib/go-1.10/bin:$PATH"
+
 py.test --cov=hil --cov-append tests/integration/migrate_ipmi_info.py
