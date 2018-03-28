d2a501542328d19b92538075f64040f190e643a9
Ian Denhardt <ian@zenhack.net>
2018-03-28 18:20:32 -0400

    Set GOROOT
    The output from the last Travis run suggests the go command is invoking
    some other commands from the wrong location.

--- ci/obmd/run_integration_tests.sh
+++ ci/obmd/run_integration_tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 set -ex
 
-# The PPA version of Go stores its executables outside of the normal path, so
-# we have to set this, otherwise we'll end up using Go 1.7.
+# Set environment variables so we use the newer version of Go, rather than
+# the 1.7 that's installed in Travis by default:
+export GOROOT=/usr/lib/go-1.10
 export PATH="/usr/lib/go-1.10/bin:$PATH"
+
+# Just in case, forget any cached command locations:
+hash -r
 
 py.test --cov=hil --cov-append tests/integration/migrate_ipmi_info.py
