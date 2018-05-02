75ae53e14f2c3865ea04aad34d58814f18df2eca
Naved Ansari <naved001@gmail.com>
2018-05-02 14:03:34 -0400

    Flesh out setting up hil from apache setup.

--- ci/apache/install.sh
+++ ci/apache/install.sh
@@ -5,27 +5,6 @@ if [ $TEST_SUITE = unit ]; then
 	exit 0
 fi
 
-# Setup configuration
-cp ci/testsuite.cfg.$DB testsuite.cfg
-sudo cp ci/apache/hil.cfg.$DB /etc/hil.cfg
-sudo chown travis:travis /etc/hil.cfg
-
-# Database Setup
-if [ $DB = postgres ]; then
-    sudo apt-get install -y python-psycopg2
-    psql --version
-    psql -c 'CREATE DATABASE hil_tests;' -U postgres
-    psql -c 'CREATE DATABASE hil;' -U postgres
-fi
-
-# Address #577 via
-# https://stackoverflow.com/questions/2192323/what-is-the-python-egg-cache-python-egg-cache
-mkdir -p ~/.python-eggs
-chmod go-w ~/.python-eggs # Eliminate "writable by group/others" warnings
-
-# Install HIL, incl. test dependencies
-pip install .[tests]
-
 # Apache Setup
 sudo chown -R travis:travis /var/www
 mkdir /var/www/hil
