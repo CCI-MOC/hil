c4e790a3a45bc7a0fd7aba76e612f6afd8dcc496
Ian Denhardt <ian@zenhack.net>
2018-05-07 12:58:05 -0400

    Make the obmd executable available for all the tests.
    instead of just the ones run from ci/obmd/run_integration_tests.sh.
    
    Previously, the final executable was in ~/go/bin, which wasn't in our
    $PATH during the usual unit tests, so many of the related tests were
    failing.
    
    The tests pass on my local machine, so hopefully this will fix CI.

--- ci/obmd/install.sh
+++ ci/obmd/install.sh
@@ -8,3 +8,17 @@ set -ex
 sudo add-apt-repository -y ppa:gophers/archive
 sudo apt-get update
 sudo apt-get -y install golang-1.10-go
+
+# Set environment variables to point at the new Go installation:
+export GOROOT=/usr/lib/go-1.10
+export PATH="$HOME/go/bin:/usr/lib/go-1.10/bin:$PATH"
+
+# Just in case, forget any cached command locations:
+hash -r
+
+# Download and build obmd:
+go get -v github.com/CCI-MOC/obmd
+
+# Copy obmd to somewhere global; this way we don't have to mess with $PATH
+# for every test that uses it:
+sudo cp ~/go/bin/obmd /usr/local/bin/
