514853de5c1d46046e172a92cd9e5d7b4cc0c3cb
Ian Denhardt <ian@zenhack.net>
2018-05-07 13:02:33 -0400

    Make running pycodestyle slightly more ergonomic.
    The same change was made to the pylint script a while back.

--- ci/pycodestyle_tests.sh
+++ ci/pycodestyle_tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# only run pycodestyle on sqlite env
-if [ $DB = sqlite ]; then
+# If we're running in CI, only run pycodestyle in the sqlite env.
+# If the DB variable is undefined we assume a developer has invoked
+# this script directly.
+if [ -z "$DB" ] || [ "$DB" = sqlite ]; then
   cd "$(dirname $0)/.."
   pycodestyle $(./ci/list_tracked_pyfiles.sh)
 fi
