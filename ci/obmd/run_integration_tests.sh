4101a931f5c850e4f7ddf49cd4539eee0e86f3d6
Ian Denhardt <ian@zenhack.net>
2018-05-31 16:29:21 -0400

    Install obmd for all tests.
    Previously obmd was getting installed too late to be used by some of the
    tests. Now we install it before running any tests, so it is available.

--- ci/obmd/run_integration_tests.sh
+++ ci/obmd/run_integration_tests.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env sh
 set -ex
 
-# download the obmd v0.1 binary
-wget https://github.com/CCI-MOC/obmd/releases/download/v0.1/obmd
-chmod +x obmd
-sudo mv obmd /usr/local/bin
-
 py.test --cov=hil --cov-append tests/integration/obmd.py
