576d514c4b6d65e2cd56eec6bd162ae9b9f27693
Naved Ansari <naved001@gmail.com>
2018-05-31 11:08:52 -0400

    Download obmd binary rather than installing it everytime the CI is run
    * This saves us time when running our CI

--- ci/obmd/run_integration_tests.sh
+++ ci/obmd/run_integration_tests.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 set -ex
 
-# Set environment variables so we use the newer version of Go, rather than
-# the 1.7 that's installed in Travis by default:
-export GOROOT=/usr/lib/go-1.10
-export PATH="$HOME/go/bin:/usr/lib/go-1.10/bin:$PATH"
-
-# Just in case, forget any cached command locations:
-hash -r
-
-go get -v github.com/CCI-MOC/obmd
+# download the obmd v0.1 binary
+wget https://github.com/CCI-MOC/obmd/releases/download/v0.1/obmd
+chmod +x obmd
+sudo mv obmd /usr/local/bin
 
 py.test tests/integration/obmd.py
